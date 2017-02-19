a926eeeb5db47476e63b1780645c8995e29288ed
wilmix <wmx.seo@gmail.com>
2017-02-19 07:36:23 -0400

    prueba de commit cambio de titulo en ingreso y cambio base url pruebas

--- application/views/ingresos/Importaciones/importaciones.php
+++ application/views/ingresos/Importaciones/importaciones.php
@@ -2,7 +2,7 @@
   <div class="col-xs-12">
     <div class="box">
     <div class="box-header with-border">
-      <h3 class="box-title">Ingreso Importaciones</h3>
+      <h3 class="box-title">Ingreso Importaciones PRUEBA COMMIT</h3>
 
       <div class="box-tools pull-right">
         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
