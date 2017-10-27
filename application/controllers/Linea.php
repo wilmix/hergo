61bd28dc1fcaf02b05e3b24ab53abe171f3f4a90
gabriel <gabriel.chavez.r@gmail.com>
2017-10-27 13:39:57 -0400

    correccion mayusculas minusculas LINEA

--- application/controllers/Linea.php
+++ application/controllers/Linea.php
@@ -67,7 +67,7 @@ class Linea extends CI_Controller {
         $this->load->view('plantilla/header.php', $this->datos);
         $this->load->view('plantilla/menu.php', $this->datos);
         $this->load->view('plantilla/headercontainer.php', $this->datos);
-        $this->load->view('administracion/Linea/linea.php', $this->datos);
+        $this->load->view('administracion/linea/linea.php', $this->datos);
         $this->load->view('plantilla/footcontainer.php', $this->datos);
         $this->load->view('plantilla/footer.php', $this->datos);
     }
