114bfb0d636b1524e3826c0b14796fe454db4fe7
Willy <wmx.seo@gmail.com>
2018-04-16 16:28:22 -0400

    arreglo modal Clietes se elimino lo inecesario

--- assets/hergo/modalClientesEgreso.js
+++ assets/hergo/modalClientesEgreso.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#form_clientes').bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-             feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-          tipo_doc: {
-                validators: {
-                    notEmpty: {
-                        message: 'Selecciona NIT o CI'
-                    }
-                }
+            // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
             },
-          carnet: {
+            fields: {
+                tipo_doc: {
                     validators: {
-                      notEmpty: {
-                        message: 'Campo obligatorio'
-                    },
+                        notEmpty: {
+                            message: 'Selecciona NIT o CI'
+                        }
+                    }
+                },
+                carnet: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Campo obligatorio'
+                        },
                         between: {
                             min: 1111,
                             max: 999999999,
@@ -26,50 +26,48 @@ $(document).ready(function() {
                         }
                     }
                 },
-            nombre_cliente: {
-                validators: {
-                        stringLength: {
-                        min: 2,
-                        message: 'Ingrese nombre válido'
-                    },
-                        notEmpty: {
-                        message: 'Campo obligatorio'
-                    }
-                }
-            },
-          direccion: {
-                validators: {
-                     stringLength: {
-                        min: 5,
-                        message: 'Ingrese dirección válida'
-                    },
-                }
-            },
-             email: {
-                validators: {
-                        emailAddress: {
-                        message: 'Ingrese un email válido'
-                    }
-                }
-            },
-          
-         
-          phone: {
+                nombre_cliente: {
                     validators: {
-                         between: {
+                        stringLength: {
+                            min: 2,
+                            message: 'Ingrese nombre válido'
+                        },
+                        notEmpty: {
+                            message: 'Campo obligatorio'
+                        }
+                    }
+                },
+                direccion: {
+                    validators: {
+                        stringLength: {
+                            min: 5,
+                            message: 'Ingrese dirección válida'
+                        },
+                    }
+                },
+                email: {
+                    validators: {
+                        emailAddress: {
+                            message: 'Ingrese un email válido'
+                        }
+                    }
+                },
+                phone: {
+                    validators: {
+                        between: {
                             min: 1111,
                             max: 99999999,
                             message: 'Igrese número de telefono valido'
                         }
                     }
                 },
-           }
+            }
         })
-        .on('success.form.bv', function(e) {
+        .on('success.form.bv', function (e) {
             e.preventDefault();
             //var valuesToSubmit = $("#form_clientes").serialize();  
-            var formData = new FormData($('#form_clientes')[0]);  
-            console.log(formData)              
+            var formData = new FormData($('#form_clientes')[0]);
+            console.log(formData)
             $.ajax({
                 url: base_url("index.php/Clientes/agregarCliente"),
                 type: 'POST',
@@ -78,61 +76,36 @@ $(document).ready(function() {
                 contentType: false,
                 processData: false,
                 success: function (returndata) {
-                   $(".mensaje_ok").html(" Los datos se guardaron correctamente");
-                   // $("#modal_ok").modal("show");
+                    $(".mensaje_ok").html(" Los datos se guardaron correctamente");
+                    // $("#modal_ok").modal("show");
                     $('#contact-form-success').show().fadeOut(10000);
                     $('#modalcliente').modal('hide');
-                   
                 }
-            }); 
+            });
         });
 });
-$(document).on("click","#botonmodalcliente",function(){
+$(document).on("click", "#botonmodalcliente", function () {
     resetForm('#form_clientes')
     $(".modal-title").html("Agregar Cliente")
     $("#bguardar").html("Guardar")
-    
 })
-$(document).on("click",".botoncerrarmodal",function(){
-   resetForm('#form_clientes')
+$(document).on("click", ".botoncerrarmodal", function () {
+    resetForm('#form_clientes')
 })
-
-
-function mostrarModal(fila)
-{
+function mostrarModal(fila) {
     console.log(fila)
     $("#id_cliente").val(fila.idCliente)
     $(".modallineatitulo").html("Editar Cliente")
-    asignarselect(fila.documentoTipo,"#tipo_doc")
+    asignarselect(fila.documentoTipo, "#tipo_doc")
     $("#carnet").val(fila.documento)
     $("#nombre_cliente").val(fila.nombreCliente)
-    asignarselect(fila.clientetipo,"#clientetipo")
+    asignarselect(fila.clientetipo, "#clientetipo")
     $("#direccion").val(fila.direccion)
     $("#phone").val(fila.telefono)
     $("#fax").val(fila.fax)
     $("#email").val(fila.email)
     $("#website").val(fila.web)
-
     $(".bguardar").html("Editar")
     $('#modalcliente').modal('show');
-    
-  
 }
 
-
-window.operateEvents = {
-    'click .editar': function (e, value, row, index) {
-      //  alert('You click like action, row: ' + JSON.stringify(row));
-      resetForm('#form_clientes')
-        mostrarModal(row)
-            $("#tarticulo").bootstrapTable('hideLoading');            
-    }
-};
-function operateFormatter(value, row, index) 
-{
-    return [
-        '<a class="editar" title="editar" data-toggle="modal" data-target="#editar" style="cursor:pointer">',
-        '<i class="fa fa-pencil"></i>',
-        '</a>  '       
-    ].join('');
-}
