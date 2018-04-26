d19c39d3739703423b074970812bd3d15994cfb3
Willy <wmx.seo@gmail.com>
2018-04-26 17:35:02 -0400

    modificacion reporte ingresos

--- application/controllers/pdf/ComprasLocales_pdf.php
+++ application/controllers/pdf/ComprasLocales_pdf.php
@@ -3,88 +3,64 @@
     require_once APPPATH."/third_party/fpdf/fpdf.php";
     require_once APPPATH."/third_party/numerosLetras/NumeroALetras.php";
     require_once APPPATH."/third_party/multicell/PDF_MC_Table.php";
-    
-
- 
 class ComprasLocales_pdf extends CI_Controller {
- 
   public function index($id=null) {
-        $this->load->model('Ingresos_model');
-        $ingreso = $this->Ingresos_model->mostrarIngresos($id)->row();
-        $params = array('tipo' => $ingreso->tipomov, 'numeroIngreso' => $ingreso->n, 'sigla'=>$ingreso->sigla, 'fechamov'=>$ingreso->fechamov, 'almacen'=>$ingreso->almacen, 'moneda'=>$ingreso->monedasigla,
-        'proveedor' =>$ingreso->nombreproveedor,'nfact' =>$ingreso->nfact,'nIngreso' =>$ingreso->ningalm,'ordenCompra' =>$ingreso->ordcomp, 'observacion' =>$ingreso->obs);
-        $this->load->library('ingresopdf/pdf', $params);
+    //CARGAR MODELO
+    $this->load->model('Ingresos_model');
+    $lineas = $this->Ingresos_model->mostrarDetalle($id);
+    $ingreso = $this->Ingresos_model->mostrarIngresos($id)->row();
+    //PARAMETROS PARA LA LIBRERIA
+    $params = array('tipo' => $ingreso->tipomov, 'numeroIngreso' => $ingreso->n, 'sigla'=>$ingreso->sigla, 'fechamov'=>$ingreso->fechamov, 'almacen'=>$ingreso->almacen, 'moneda'=>$ingreso->monedasigla,
+    'proveedor' =>$ingreso->nombreproveedor,'nfact' =>$ingreso->nfact,'nIngreso' =>$ingreso->ningalm,'ordenCompra' =>$ingreso->ordcomp, 'observacion' =>$ingreso->obs);
+    $this->load->library('ingresopdf/pdf', $params);
 
-        $lineas = $this->Ingresos_model->mostrarDetalle($id); //********cambiar nombre de class de modelo
-        
-        $this->pdf = new Pdf($params);
-        /*echo '<pre>';
-        print_r ($lineas);
-        echo '</pre>';*/
-        
-        // Agregamos una página
-        $this->pdf->AddPage('L','Letter');
-        $this->pdf->SetX(10);
-        // Define el alias para el número de página que se imprimirá en el pie
-        $this->pdf->AliasNbPages();
-        
-        $year = date('y',strtotime($ingreso->fechamov));
- 
-        $this->pdf->SetTitle($ingreso->tipomov . " - ". $ingreso->n . " / " . $year);
-        $this->pdf->SetLeftMargin(15);
-        $this->pdf->SetRightMargin(15);
-
-    
-
-        $this->pdf->SetX(10);
-        $this->pdf->SetFont('Arial', '', 8);
-        // La variable $x se utiliza para mostrar un número consecutivo
-        $n = 1;
-        
-
-        $totalInventario=0;
-        $totalDocumento=0;
-        
-        foreach ($lineas->result() as $linea) {
+      $this->pdf = new Pdf($params);
+      $this->pdf->AddPage('L','Letter');
+      //$this->pdf->SetX(10);
+      $this->pdf->AliasNbPages();
+      
+      $year = date('y',strtotime($ingreso->fechamov));
+      $this->pdf->SetTitle($ingreso->tipomov . " - ". $ingreso->n . " / " . $year);
+      $this->pdf->SetLeftMargin(10);
+      $this->pdf->SetRightMargin(10);
+      $this->pdf->SetFont('Arial', '', 8);
+      
+      $n = 1;
+      $totalInventario=0;
+      $totalDocumento=0;
+      foreach ($lineas->result() as $linea) {
         $totalInventario += $linea->total;
         $totalDocumento += $linea->totaldoc;
         $costoUniDoc= $linea->totaldoc/$linea->cantidad;
 
         $this->pdf->SetX(10);
         $this->pdf->SetFillColor(255,255,255);
-                    $this->pdf->Cell(7,5,$n++,'',0,'C',0); ///NUMERO DE FILA
-                    $this->pdf->Cell(18,5,number_format($linea->cantidad, 2, ".", ","),'',0,'R',0);
-                    $this->pdf->Cell(10,5,$linea->Unidad,'',0,'C',0);
-                    $this->pdf->Cell(18,5,$linea->CodigoArticulo,'',0,'C',0);
-                    $this->pdf->Cell(120,5,utf8_decode($linea->Descripcion),0,0,'L',0);
-                    $this->pdf->Cell(20,5,number_format($costoUniDoc, 2, ".", ","),0,0,'R',1);
-                    $this->pdf->Cell(20,5,number_format($linea->totaldoc, 2, ".", ","),'',0,'R',1);
-        $this->pdf->SetFillColor(245,245,245);
-                    $this->pdf->Cell(20,5,number_format($linea->punitario, 2, ".", ","),'',0,'R',1);
-                    $this->pdf->Cell(20,5,number_format($linea->total, 2, ".", ","),'',0,'R',1);   //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
-
-        //Se agrega un salto de linea
+            $this->pdf->Cell(7,5,$n++,'',0,'C',0); ///NUMERO DE FILA
+            $this->pdf->Cell(18,5,number_format($linea->cantidad, 2, ".", ","),'',0,'R',0);
+            $this->pdf->Cell(10,5,$linea->Unidad,'',0,'C',0);
+            $this->pdf->Cell(18,5,$linea->CodigoArticulo,'',0,'C',0);
+            $this->pdf->Cell(120,5,utf8_decode($linea->Descripcion),0,0,'L',0);
+            $this->pdf->Cell(20,5,number_format($costoUniDoc, 2, ".", ","),0,0,'R',1);
+            $this->pdf->Cell(20,5,number_format($linea->totaldoc, 2, ".", ","),'',0,'R',1);
+            $this->pdf->SetFillColor(245,245,245);
+            $this->pdf->Cell(20,5,number_format($linea->punitario, 2, ".", ","),'',0,'R',1);
+            $this->pdf->Cell(20,5,number_format($linea->total, 2, ".", ","),'',0,'R',1);   //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
         $this->pdf->Ln(5);
         $this->pdf->SetX(10);
-        }
-        $this->pdf->Ln(2);
-        // TOTALES
-        $this->pdf->SetX(10);
-        $this->pdf->SetFont('Times','BI',10);
-        $this->pdf->SetFillColor(232,232,232); 
-        $this->pdf->Cell(13,7,'Total: ','1',0,'L',1);
-        $this->pdf->SetFont('Times','I',10);
-        $literal = NumeroALetras::convertir($totalInventario, 'BOLIVIANOS', 'CENTAVOS');
-        
-        $this->pdf->Cell(160,7,$literal,'1',0,'l',1);
-        $this->pdf->SetFont('Arial','B',9);
-        $this->pdf->Cell(40,7,number_format($totalDocumento, 2, ".", ","),'1',0,'R',1); //TOTAL DOCUMENTO
-        $this->pdf->Cell(40,7,number_format($totalInventario, 2, ".", ","),'1',0,'R',1); //TOTAL INVENTARIO
-
-
-
-
-        //guardar
-        $this->pdf->Output($ingreso->sigla . " - ". $ingreso->n . " - " . $year, 'I');
+      }
+      $this->pdf->Ln(2);
+     
+      // TOTALES
+      $this->pdf->SetFont('Times','BI',10);
+      $this->pdf->SetFillColor(232,232,232); 
+      $this->pdf->Cell(13,7,'Total: ','1',0,'L',1);
+      $this->pdf->SetFont('Times','I',10);
+      $literal = NumeroALetras::convertir($totalInventario, 'BOLIVIANOS', 'CENTAVOS');
+      $this->pdf->Cell(160,7,$literal,'1',0,'l',1);
+      $this->pdf->SetFont('Arial','B',9);
+      $this->pdf->Cell(40,7,number_format($totalDocumento, 2, ".", ","),'1',0,'R',1); //TOTAL DOCUMENTO
+      $this->pdf->Cell(40,7,number_format($totalInventario, 2, ".", ","),'1',0,'R',1); //TOTAL INVENTARIO
+      //guardar
+      $this->pdf->Output($ingreso->sigla . " - ". $ingreso->n . " - " . $year, 'I');
   }
 }
--- application/libraries/Ingresopdf/pdf.php
+++ application/libraries/Ingresopdf/pdf.php
@@ -9,8 +9,6 @@
             parent::__construct();
             $this->datos = $params;
         }
-        //CellFitSpace( ) 
-        // El encabezado del PDF
         public function Header() {
             $tipo = $this->datos['tipo'];
             $numeroIngreso = $this->datos['numeroIngreso'];
@@ -22,24 +20,18 @@
             $nIngreso = $this->datos['nIngreso'];
             $ordenCompra = $this->datos['ordenCompra'];
             $sigla = $this->datos['sigla'];
-
-            $this->SetY(10);
-            $this->SetX(10);
+            //TITULO
+            $this->SetXY(10,10);
             $this->Image('images/hergo.jpeg', 10, 10, 45 );
             $this->SetFont('Arial','B',10);
             $this->SetXY(15,20);
             $this->Cell(40,6, $almacen,0,0,'C');
-            
             $this->SetFont('Arial','B',10);
             $this->SetXY(10,10);
             $this->SetFont('Arial','B',18);
-            $this->Cell(0,8, 'NOTA DE INGRESOS',0,1,'C'); //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
-            /*$this->SetFont('Arial','B',13);
-            $this->Cell(0,6, $almacen,0,1,'C');*/
+            $this->Cell(0,8, 'NOTA DE INGRESOS',0,1,'C'); 
             $this->SetFont('Arial','BU',15);
             $this->Cell(0,8, $tipo,0,0,'C');
-
-
             $this->SetXY(220,12);
             $this->SetFont('Arial','B',15);
             $this->Cell(45,8, $sigla . " - " .$numeroIngreso,1,1,'C');
@@ -47,59 +39,60 @@
             $this->SetFont('Arial','B',12);
             $this->Cell(45,8, $fechaMovimiento,1,0,'C');
             $this->Ln(10);
-
             
-            //****ENCABEZADO****
-            
-            $this->SetX(15);
-            $this->SetFont('Arial','',10);
-            //proveedor
-            $this->Cell(20,10, 'Proveedor: ',0,0,'');
-            $this->Cell(85, 10, $proveedor, 0,0,'L');
-            //factura
-            $this->Cell(20,10, 'Factura: ',0,0,'');
-            $this->Cell(20, 10, $nfact, 0,0,'L');
-            // N° ingreso
-            $this->Cell(25,10, 'Nro Ingreso: ',0,0,'');
-            $this->Cell(20, 10, $nIngreso, 0,0,'L');
-            // Orden de compra
-            $this->Cell(25,10, 'Orden Compra: ',0,0,'');
-            $this->Cell(20, 10, $ordenCompra, 0,1,'L');
-        // $this->Ln(10);
-            //ENCABEZADO TABLA
-            $this->SetX(10);
-            $this->SetFillColor(255,255,255);
-            $this->SetFont('Arial','B',9); 
-            $this->Cell(173,6,'',0,0,'C',1);
-            $this->SetFillColor(232,232,232);
-            $this->Cell(40,6,'DOCUMENTO',1,0,'C',1);
-            $this->Cell(40,6,'INVENTARIO ',1,0,'C',1);
-            $this->Ln(6);
-            $this->Cell(7,6,'N',1,0,'C',1);
-            $this->Cell(18,6,'CANT',1,0,'c',1);
-            $this->Cell(10,6,'UNID',1,0,'C',1);
-            $this->Cell(18,6,'CODIGO',1,0,'C',1);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
-            $this->Cell(120,6,'DESCRIPCION',1,0,'C',1);
-            $this->Cell(20,6,'P/U',1,0,'R',1);
-            $this->Cell(20,6,'TOTAL',1,0,'R',1);
-            $this->Cell(20,6,'P/U',1,0,'R',1);
-            $this->Cell(20,6,'TOTAL ',1,0,'R',1);
-            $this->Ln(8);
-       }
+                //****ENCABEZADO****
+                $this->SetX(15);
+                //proveedor
+                $this->SetFont('Arial','B',10);
+                $this->Cell(20,10, 'Proveedor: ',0,0,'');
+                $this->SetFont('Arial','',10);
+                $this->Cell(85, 10, $proveedor, 0,0,'L');
+                //factura
+                $this->SetFont('Arial','B',10);
+                $this->Cell(20,10, 'Factura: ',0,0,'');
+                $this->SetFont('Arial','',10);
+                $this->Cell(20, 10, $nfact, 0,0,'L');
+                // N° ingreso
+                $this->SetFont('Arial','B',10);
+                $this->Cell(25,10, 'Nro Ingreso: ',0,0,'');
+                $this->SetFont('Arial','',10);
+                $this->Cell(20, 10, $nIngreso, 0,0,'L');
+                // Orden de compra
+                $this->SetFont('Arial','B',10);
+                $this->Cell(30,10, 'Orden Compra: ',0,0,'');
+                $this->SetFont('Arial','',10);
+                $this->Cell(20, 10, $ordenCompra, 0,1,'L');
+                $this->SetX(10);
+                    //ENCABEZADO TABLA
+                    $this->SetFillColor(255,255,255);
+                    $this->SetFont('Arial','B',9); 
+                    $this->Cell(173,6,'',0,0,'C',1);
+                    $this->SetFillColor(232,232,232);
+                    $this->Cell(40,6,'DOCUMENTO',1,0,'C',1);
+                    $this->Cell(40,6,'INVENTARIO ',1,0,'C',1);
+                    $this->Ln(6);
+                    $this->Cell(7,6,'N',1,0,'C',1);
+                    $this->Cell(18,6,'CANT',1,0,'c',1);
+                    $this->Cell(10,6,'UNID',1,0,'C',1);
+                    $this->Cell(18,6,'CODIGO',1,0,'C',1);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
+                    $this->Cell(120,6,'DESCRIPCION',1,0,'C',1);
+                    $this->Cell(20,6,'P/U',1,0,'R',1);
+                    $this->Cell(20,6,'TOTAL',1,0,'R',1);
+                    $this->Cell(20,6,'P/U',1,0,'R',1);
+                    $this->Cell(20,6,'TOTAL ',1,0,'R',1);
+                    $this->Ln(8);
+        }
 
-       // El pie del pdf
         public function Footer(){
             $observacion = $this->datos['observacion'];
-            //posicion a 1.5 cm del final
             $this->SetFont('Arial','I', 10);
             $this->SetY(-20);
             $this->Cell(30,10, 'Observaciones: ',0,0,'L');
             $this->Cell(210, 10, $observacion, 0,0,'L');
-            $this->SetY(-12);
-            //fuente
-            $this->SetFont('Arial','I', 8);
-            //numero de pagina
-            $this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
+                //NUMERO PIED PAGINA
+                $this->SetY(-12);
+                $this->SetFont('Arial','I', 8);
+                $this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
         }
     }
 ?>
