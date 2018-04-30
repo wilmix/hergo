7d0717d1ecd713b95c1818e5a49d82176fbf335a
Willy <wmx.seo@gmail.com>
2018-04-30 11:31:42 -0400

    cambio config para servidor

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/hergo/';
+$config['base_url'] = 'http://45.55.252.151/hergo/';
 /*
 |--------------------------------------------------------------------------
 | Index File
