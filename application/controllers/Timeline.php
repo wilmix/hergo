483e9ee215505ee0fbfb0e3c4cccb1e92f790de6
Willy <wmx.seo@gmail.com>
2019-12-06 13:07:46 -0400

    creacion modulo timeline

---
+++ application/controllers/Timeline.php
@@ -0,0 +1,103 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Timeline extends CI_Controller
+{
+	private $datos;
+	public function __construct()
+	{
+		parent::__construct();
+		/*******/
+		$this->load->library('LibAcceso');
+	
+		/*******/
+		$this->load->helper('url');
+		//$this->load->model("Roles_model");
+		$this->load->model("Ingresos_model");
+		$this->load->helper('date');
+		date_default_timezone_set("America/La_Paz");
+		$this->cabeceras_css=array(
+				base_url('assets/bootstrap/css/bootstrap.min.css'),
+				base_url("assets/fa/css/font-awesome.min.css"),
+				base_url("assets/dist/css/AdminLTE.min.css"),
+				base_url("assets/dist/css/skins/skin-blue.min.css"),
+				base_url("assets/hergo/estilos.css"),
+				base_url('assets/plugins/table-boot/css/bootstrap-table.css'),
+				base_url('assets/plugins/table-boot/plugin/select2.min.css'),
+				base_url('assets/plugins/table-boot/plugin/bootstrap-table-group-by.css'),	
+				base_url('assets/plugins/table-boot/plugin/bootstrap-table-sticky-header.css'),				
+				base_url('assets/sweetalert/sweetalert2.min.css'),
+		);
+		$this->cabecera_script=array(
+				base_url('assets/plugins/jQuery/jquery-2.2.3.min.js'),
+				base_url('assets/bootstrap/js/bootstrap.min.js'),
+				base_url('assets/dist/js/app.min.js'),
+				base_url('assets/plugins/validator/bootstrapvalidator.min.js'),
+				base_url('assets/plugins/table-boot/js/bootstrap-table.js'),
+				base_url('assets/plugins/table-boot/js/bootstrap-table-es-MX.js'),
+				base_url('assets/plugins/table-boot/js/bootstrap-table-export.js'),
+				base_url('assets/plugins/table-boot/js/tableExport.js'),
+				base_url('assets/plugins/table-boot/js/xlsx.core.min.js'),
+				base_url('assets/plugins/table-boot/js/bootstrap-table-filter.js'),
+				base_url('assets/plugins/table-boot/plugin/select2.min.js'),
+				base_url('assets/plugins/table-boot/plugin/bootstrap-table-select2-filter.js'),
+				base_url('assets/plugins/table-boot/plugin/bootstrap-table-group-by.js'),
+				base_url('assets/plugins/table-boot/plugin/FileSaver.min.js'),
+				base_url('assets/plugins/table-boot/plugin/bootstrap-table-sticky-header.js'),
+        		base_url('assets/plugins/daterangepicker/moment.min.js'),
+        		base_url('assets/plugins/slimscroll/slimscroll.min.js'),        		
+        		base_url('assets/sweetalert/sweetalert2.min.js'),
+        		
+
+		);
+		$this->datos['nombre_usuario']= $this->session->userdata('nombre');
+		$this->datos['almacen_usuario']= $this->session->userdata['datosAlmacen']->almacen;
+		$this->datos['user_id_actual']=$this->session->userdata['user_id'];
+		$this->datos['nombre_actual']=$this->session->userdata['nombre'];
+		$this->datos['almacen_actual']=$this->session->userdata['datosAlmacen']->almacen;
+		$this->datos['id_Almacen_actual']=$this->session->userdata['datosAlmacen']->idalmacen;
+		$hoy = date('Y-m-d');
+		$tipoCambio = $this->Ingresos_model->getTipoCambio($hoy);
+		if ($tipoCambio) {
+			$tipoCambio = $tipoCambio->tipocambio;
+			$this->datos['tipoCambio'] = $tipoCambio;
+		} else {
+			$this->datos['tipoCambio'] = 'No se tiene tipo de cambio para la fecha';
+		}
+			if($this->session->userdata('foto')==NULL)
+				$this->datos['foto']=base_url('assets/imagenes/ninguno.png');
+			else
+				$this->datos['foto']=base_url('assets/imagenes/').$this->session->userdata('foto');
+	}
+
+	public function index(){
+		if(!$this->session->userdata('logeado'))
+			redirect('auth', 'refresh');
+
+			$this->datos['menu']="Timeline";
+			$this->datos['opcion']="Timeline";
+			$this->datos['titulo']="Timeline";
+
+			$this->datos['cabeceras_css']= $this->cabeceras_css;
+			$this->datos['cabeceras_script']= $this->cabecera_script;
+
+			/**************FUNCION***************/
+			$this->datos['cabeceras_script'][]=base_url('assets/hergo/funciones.js');
+			$this->datos['cabeceras_script'][]=base_url('assets/hergo/timeline.js'); 			
+			/**************INPUT MASK***************/
+			$this->datos['cabeceras_script'][]=base_url('assets/plugins/inputmask/inputmask.js');
+			$this->datos['cabeceras_script'][]=base_url('assets/plugins/inputmask/inputmask.numeric.extensions.js');
+            $this->datos['cabeceras_script'][]=base_url('assets/plugins/inputmask/jquery.inputmask.js');
+            //$this->datos['users']=$this->Roles_model->retornar_users();
+
+			$this->load->view('plantilla/head.php',$this->datos);
+			$this->load->view('plantilla/header.php',$this->datos);
+			$this->load->view('plantilla/menu.php',$this->datos);
+			$this->load->view('plantilla/headercontainer.php',$this->datos);
+			$this->load->view('timeline.php',$this->datos);
+			$this->load->view('plantilla/footcontainer.php',$this->datos);
+			$this->load->view('plantilla/footer.php',$this->datos);
+	} 
+
+
+
+}
---
+++ application/views/timeline.php
@@ -0,0 +1,198 @@
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-body">
+          <div class="text-center">
+          <div class="container">
+          <h2>Timeline </h2>
+          </div>
+            <h4 class="text-center">A continuación se muestran el trabajo realizado en: <br>
+                <dl>
+                <dt>
+                    <span class="fa fa-check-square" style="color:orange"></span>  Mejoras readtzadas 
+                </dt>
+                <dt>
+                    <span class="fa fa-wrench" style="color:blue"></span>  Correcciones o ajustes reportadas por los compañeros. 
+                </dt>
+                <dt>
+                    <span class="fa fa-plus" style="color:green"></span>  Nuevas características o módulos 
+                </dt>
+                
+                </dl>
+
+            </h4>
+          </div>
+        
+
+            <section class="content">
+                <div class="row">
+                    <div class="col-md-12">
+                    <!-- The time line -->
+                        <ul class="timeline">
+                            <!-- timeline time label -->
+                            <li class="time-label">
+                                <span class="bg-red">06 Dic. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-plus bg-green"></i>
+
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Creación del módulo Timeline</a></h3>
+                                        <div class="timeline-body">
+                                        Se creó el módulo Timeline donde se muestra el trabajo realizado por el area de sistemas visualizable amigablemente en una linea temporal para el conocimiento de toda la empresa.
+                                        </div>
+                                    </div>
+                                </li>
+
+
+                            <li class="time-label">
+                                <span class="bg-red">05 Dic. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-wrench bg-blue"></i>
+
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Correcciòn en Kardex Cliente</a></h3>
+                                        <div class="timeline-body">
+                                        Se corrigió y optimizo la búsqueda de clientes en el Kardex Individual Cliente. Ahora funciona mucho mas fluido. Teniendo en cuenta que tenemos casi 18.000 clientes a nivel nacional.
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="fa fa-check-square bg-orange"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header no-border"><a href="#">Mejora en Grafico Ventas</a> </h3>
+                                        <div class="timeline-body">
+                                        Se mejoro el grafico de ventas añadiendo ventas de todas las sucursales y a nivel de nacional. Para hacer visible este grafico deberá asignarse el respectivo permiso en roles.
+                                        </div>
+                                    </div>
+                                </li>
+                            <li class="time-label">
+                                <span class="bg-red">27 Nov. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-plus bg-green"></i>
+
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Creación Nuevo modulo Prueba Kardex</a></h3>
+                                        <div class="timeline-body">
+                                        Se agrego un nuevo reporte Prueba Kardex para realizar la prueba y dar mayor información en el trabajo entre inventarios y sistema contable.
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <i class="fa fa-plus bg-green"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header no-border"><a href="#">Creación Nuevo Reporte Estado de Ventas y Costo</a> </h3>
+                                        <div class="timeline-body">
+                                        Se añadió un nuevo reporte Estado de Ventas y Costo para resolver la diferencias con sistema contable.
+                                        </div>
+                                    </div>
+                                </li>
+
+
+                            <li class="time-label">
+                                <span class="bg-red">25 Sep. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-plus bg-green"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Agregó Total Ingresos</a></h3>
+                                        <div class="timeline-body">
+                                        Se agrego total factura en Ingresos Consulta para mejor revisión.
+                                        </div>
+                                    </div>
+                                </li>
+                            
+
+                            <li class="time-label">
+                                <span class="bg-red">24 Sep. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-wrench bg-blue"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Corrigió glosa en ingresos</a></h3>
+                                        <div class="timeline-body">
+                                        Se corrigió carácteres extraños en glosa de ingresos.
+                                        </div>
+                                    </div>
+                                </li>
+
+
+                            <li class="time-label">
+                                <span class="bg-red">17 Sep. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-wrench bg-blue"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Añadió Validación Cliente</a></h3>
+                                        <div class="timeline-body">
+                                        Se corrigió error encontrado a la posibilidad de crear clientes duplicados reportado por los compañeors.
+                                        </div>
+                                    </div>
+                                </li>  
+
+                            <li class="time-label">
+                                <span class="bg-red">26 Ago. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-plus bg-green"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Creación Accesos a modulos Reportes</a></h3>
+                                        <div class="timeline-body">
+                                        Se creo restriciones en accesos a los siguientes módulos: 
+                                        <blockquote>
+                                            <ul class="list-group">
+                                                <li>Ventas Cliente Item</li>
+                                                <li>Cliente Ventas  Item</li>
+                                                <li>Reporte Ingresos</li>
+                                                <li>Reporte Egresos</li>
+                                                <li>Reporte Facturas</li>
+                                                <li>Reporte Pagos</li>
+                                            </ul>
+                                            </blockquote>
+                                        Se deben activar permisos en Roles para su acceso.
+                                        </div>
+                                    </div>
+                                </li>      
+
+
+                            <li class="time-label">
+                                <span class="bg-red">25 Jul. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-wrench bg-blue"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Correción Busqueda Reporte Facturas Pendientes de Pago</a></h3>
+                                        <div class="timeline-body">
+                                        Se optimizó la manera de busqueda de fechas y visualización del reporte.
+                                        </div>
+                                    </div>
+                                </li>                              
+
+
+                            <li class="time-label">
+                                <span class="bg-red">23 Jul. 2019</span>
+                            </li>
+                                <li>
+                                    <i class="fa fa-wrench bg-blue"></i>
+                                    <div class="timeline-item">
+                                        <h3 class="timeline-header"><a href="#">Correción PDF Egresos</a></h3>
+                                        <div class="timeline-body">
+                                        Se corrigió la fecha de pago en generacion de Nota de Entrega o Ventas Caja en fecha de pago cuando se guardaba vacia.
+                                        </div>
+                                    </div>
+                                </li>  
+
+                        </ul>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </section>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
+</div>
