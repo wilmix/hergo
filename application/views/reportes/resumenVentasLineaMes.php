09b8d0b4abae0001d3014723fd735611e4c09d1a
Willy <wms.seo@gmail.com>
2020-04-06 11:03:27 -0400

    restriccion nacional Reportes resumen ventas linea mes

--- application/views/reportes/resumenVentasLineaMes.php
+++ application/views/reportes/resumenVentasLineaMes.php
@@ -2,49 +2,48 @@
   <div class="col-xs-12">
     <div class="box">
         <div class="box-body">
-        <button class="btn btn-success pull-right" id="export" data-toggle="tooltip" title="Excel"><i class="far fa-file-excel"> </i> Excel </button>
-        <button onclick="window.print();" class="btn btn-primary pull-right" ><i class="fa fa-print"> </i> Imprimir</button>
-        <hr>
-            <div id="toolbar2" class="form-inline">
-
-            <button  type="button" class="btn btn-primary btn-sm" id="fechapersonalizada">
-              <span>
-                <i class="fa fa-calendar"></i> Fecha
-              </span>
-                <i class="fa fa-caret-down"></i>
-            </button>
-              <select   class="btn btn-primary btn-sm" data-style="btn-primary" id="almacen_filtro" name="almacen_filtro">
-              <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
-                <?php foreach ($almacen->result_array() as $fila): ?>
-                <option value=<?= $fila['idalmacen'] ?> ><?= $fila['almacen'] ?></option>
-                <?php endforeach ?>
-                <option value="">TODOS</option>
-              </select>
-              
-              <select id="moneda" class="btn btn-primary btn-sm">
+          <button class="btn btn-success pull-right" id="export" data-toggle="tooltip" title="Excel"><i class="far fa-file-excel"> </i> Excel </button>
+          <button onclick="window.print();" class="btn btn-primary pull-right" ><i class="fa fa-print"> </i> Imprimir</button>
+          <hr>
+              <div id="toolbar2" class="form-inline">
+                <button  type="button" class="btn btn-primary btn-sm" id="fechapersonalizada">
+                  <span>
+                    <i class="fa fa-calendar"></i> Fecha
+                  </span>
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <select   class="btn btn-primary btn-sm" data-style="btn-primary" id="almacen_filtro" name="almacen_filtro">
+                    <?php if ($grupsOfUser == 'Nacional') : ?>
+                        <?php foreach ($almacen->result_array() as $fila): ?>
+                          <option value=<?= $fila['idalmacen'] ?> ><?= $fila['almacen'] ?></option>
+                        <?php endforeach ?>
+                          <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
+                          <option value="">TODOS</option>
+                    <?php else : ?>
+                        <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
+                    <?php endif; ?>
+                </select>
+                <select id="moneda" class="btn btn-primary btn-sm">
                   <option value="0">BOB</option>
                   <option value="1">$U$</option>
-              </select>
-
-              <button  type="button" class="btn btn-primary btn-sm" id="refresh">
+                </select>
+                <button  type="button" class="btn btn-primary btn-sm" id="refresh">
                   <span>
-                  <i class="fa fa-share-square"></i>
+                    <i class="fa fa-share-square"></i>
                   </span>
-              </button>
-            </div>
+                </button>
+              </div>
               <div class="text-center">
-              <h2>RESUMEN VENTAS LINEA MES - <span id="tituloAlmacen"></span></h2>
-              <h4 id="ragoFecha"></h4>
-              <h5 id="monedaTitulo"></h5>
-            </div>
-            <table 
-              id="tablaResumenVentasLineaMes" 
-              data-toolbar="#toolbar2"
-              data-toggle="table">
-            </table>
-
-          </div>
-
+                <h2>RESUMEN VENTAS LINEA MES - <span id="tituloAlmacen"></span></h2>
+                <h4 id="ragoFecha"></h4>
+                <h5 id="monedaTitulo"></h5>
+              </div>
+              <table 
+                id="tablaResumenVentasLineaMes" 
+                data-toolbar="#toolbar2"
+                data-toggle="table">
+              </table>
+        </div>
       <!-- /.box-body -->
     </div>
     <!-- /.box -->
