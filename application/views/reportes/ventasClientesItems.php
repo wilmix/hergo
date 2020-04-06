eab93708cc31517a5167658c82e06a24484569fa
Willy <wms.seo@gmail.com>
2020-04-06 12:06:14 -0400

    restriccion nacional Reportes item/clientes - clientes/item

--- application/views/reportes/ventasClientesItems.php
+++ application/views/reportes/ventasClientesItems.php
@@ -2,9 +2,9 @@
     <div class="col-xs-12">
         <div class="box">
             <div class="box-body">
-            <button class="btn btn-success pull-right" id="export" data-toggle="tooltip" title="Excel"><i class="far fa-file-excel"> </i> Excel </button>
-            <button onclick="window.print();" class="btn btn-primary pull-right" ><i class="fa fa-print"> </i> Imprimir</button>
-            <hr>
+                <button class="btn btn-success pull-right" id="export" data-toggle="tooltip" title="Excel"><i class="far fa-file-excel"> </i> Excel </button>
+                <button onclick="window.print();" class="btn btn-primary pull-right" ><i class="fa fa-print"> </i> Imprimir</button>
+                <hr>
                 <div id="toolbar2" class="form-inline">
                     <button  type="button" class="btn btn-primary btn-sm" id="fechapersonalizada">
                         <span>
@@ -12,22 +12,23 @@
                         </span>
                         <i class="fa fa-caret-down"></i>
                     </button>
-           
                     <select   class="btn btn-primary btn-sm" data-style="btn-primary" id="almacen_filtro" name="almacen_filtro">
-                        <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
-                        <?php foreach ($almacen->result_array() as $fila): ?>
-                        <option value=<?= $fila['idalmacen'] ?> ><?= $fila['almacen'] ?></option>
-                        <?php endforeach ?>
-                        <option value="">TODOS</option>
+                        <?php if ($grupsOfUser == 'Nacional') : ?>
+                            <?php foreach ($almacen->result_array() as $fila): ?>
+                            <option value=<?= $fila['idalmacen'] ?> ><?= $fila['almacen'] ?></option>
+                            <?php endforeach ?>
+                            <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
+                            <option value="">TODOS</option>
+                        <?php else : ?>
+                            <option value=<?= $id_Almacen_actual ?> selected="selected"><?= $almacen_actual ?></option>
+                        <?php endif; ?>
                     </select>
-
                     <button  type="button" class="btn btn-primary btn-sm" id="refresh">
                         <span>
                         <i class="fa fa-share-square"></i>
                         </span>
                     </button>
                 </div>
-
                 <div class="text-center">
                     <h2>Movimiento  Items Clientes
                         <span id="item"></span>
