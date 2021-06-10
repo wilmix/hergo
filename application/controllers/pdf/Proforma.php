1194525ff6ee5162f1ed480781db5bdc16e80181
Willy <wmx.seo@gmail.com>
2021-06-10 16:05:49 -0400

    pdf articulos agregados

--- application/controllers/pdf/Proforma.php
+++ application/controllers/pdf/Proforma.php
@@ -9,7 +9,9 @@ class Proforma extends CI_Controller {
     $this->load->model('Proforma_model');
     $proforma = $this->Proforma_model->getProforma($id);
     $items = $this->Proforma_model->getProformaItems($id);
-    $params = array(
+    $params = get_object_vars($proforma);
+    //print_r($params);die();
+    /* $params = array(
         'almacen' => $proforma->almacen,
         'fecha' => $proforma->fecha,
         'num' => $proforma->num,
@@ -21,7 +23,7 @@ class Proforma extends CI_Controller {
         'porcentajeDescuento' => $proforma->porcentajeDescuento,
         'autor' => $proforma->autor,
         'email' => $proforma->email,
-    );
+    ); */
     $year = date('y',strtotime($proforma->fecha));
 
     
@@ -29,7 +31,7 @@ class Proforma extends CI_Controller {
         $this->pdf = new Proforma_lib($params);
         $this->pdf->AddPage('P','Letter');
         $this->pdf->AliasNbPages();
-        $this->pdf->SetAutoPageBreak(true,40); //40
+        $this->pdf->SetAutoPageBreak(true,55); //40
         $this->pdf->SetTitle('PRO' . '-' .$proforma->num. '-' . $year);
         $this->items($items,$proforma);
 
@@ -37,30 +39,9 @@ class Proforma extends CI_Controller {
   }
   public function items($items,$proforma)
   {
-        
-        //print_r(($items)); die();
         $l = 0;
-        $this->pdf->SetXY(10,50);
-        $this->pdf->Ln(1);
-        $this->pdf->SetFillColor(235,235,235);
-        $this->pdf->SetFont('Arial','B',6); 
-                //$this->pdf->Cell(5,6,'N',$l,0,'C',1);
-                $this->pdf->SetFillColor(20,60,190);
-                    $this->pdf->SetFont('Arial','B',8); 
-                    $this->pdf->SetTextColor(255,255,255);
-                    $this->pdf->Cell(5,6,utf8_decode('Nº'),0,0,'C',1);
-                    $this->pdf->Cell(25,6,'Imagen',0,0,'C',1);
-                    $this->pdf->Cell(15,6,utf8_decode('Código'),0,0,'C',1);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
-                    $this->pdf->Cell(45,6,utf8_decode('Descripción'),0,0,'C',1);
-                    $this->pdf->Cell(15,6,'Marca',0,0,'C',1);
-                    $this->pdf->Cell(15,6,'Industria',0,0,'C',1);
-                    $this->pdf->Cell(15,6,'Entrega',0,0,'C',1);
-                    $this->pdf->Cell(15,6,'Cant.',0,0,'R',1);
-                    $this->pdf->Cell(10,6,'Uni.',0,0,'C',1);
-                    $this->pdf->Cell(20,6,'P/U',0,0,'R',1);
-                    $this->pdf->Cell(20,6,'Total',0,0,'R',1);
-
-        $this->pdf->Ln(6);
+ 
+        //$this->pdf->Ln(6);
         $n = 1;
         $total = 0;
         foreach ($items as $item) {
@@ -68,22 +49,23 @@ class Proforma extends CI_Controller {
           $this->pdf->SetFillColor(255,255,255);
           $this->pdf->SetTextColor(0,0,0);
           $this->pdf->SetFont('Arial','',8); 
-          $this->pdf->Cell(5,5,$n++,$l,0,'C',0); 
-          $url_img= $item->img ? 'assets/img_articulos/' . $item->img : 'assets/img_articulos/hergo.jpg';
-          $this->pdf->Cell(25,5,$this->pdf->Image($url_img, $this->pdf->GetX() + 5, $this->pdf->GetY() + 1, 15 ),$l,0,'C',0);
-          $this->pdf->Cell(15,5,utf8_decode($item->codigo),$l,0,'C',0);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
-          $this->pdf->MultiCell(45,5,iconv('UTF-8', 'windows-1252', ($item->descripcion)),$l,'L',0);
-          $this->pdf->SetXY(100,$this->pdf->GetY()-5);
-          $this->pdf->Cell(15,5,$item->marca,$l,0,'C',1);
-          $this->pdf->Cell(15,5,$item->industria,$l,0,'C',1);
-          $this->pdf->Cell(15,5,$item->tiempoEntrega,$l,0,'C',1);
-          $this->pdf->Cell(15,5,number_format($item->cantidad, 2, ".", ","),$l,0,'R',0);
-          $this->pdf->Cell(10,5,$item->uni,$l,0,'C',1);
-          $this->pdf->Cell(20,5,number_format($item->precio, 2, ".", ","),$l,0,'R',0);
-          $this->pdf->Cell(20,5,number_format($item->total, 2, ".", ","),$l,0,'R',0);
+            $this->pdf->Cell(5,5,$n++,$l,0,'C',0); 
+            $url_img= $item->img ? 'assets/img_articulos/' . $item->img : 'assets/img_articulos/hergo.jpg';
+            $this->pdf->Cell(25,5,$this->pdf->Image($url_img, $this->pdf->GetX() + 5, $this->pdf->GetY(), 12 ),$l,0,'C',0);
+            $this->pdf->Cell(15,5,utf8_decode($item->codigo),$l,0,'C',0);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
+            $this->pdf->MultiCell(45,5,iconv('UTF-8', 'windows-1252', ($item->descripcion)),$l,'L',0);
+            $this->pdf->SetXY(100,$this->pdf->GetY()-5);
+            $this->pdf->Cell(15,5,$item->marca,$l,0,'C',1);
+            $this->pdf->Cell(15,5,$item->industria,$l,0,'C',1);
+            $this->pdf->Cell(15,5,$item->tiempoEntrega,$l,0,'C',1);
+            $this->pdf->Cell(15,5,number_format($item->cantidad, 2, ".", ","),$l,0,'R',0);
+            $this->pdf->Cell(10,5,$item->uni,$l,0,'R',1);
+            $this->pdf->Cell(20,5,number_format($item->precio, 2, ".", ","),$l,0,'R',0);
+            $this->pdf->Cell(20,5,number_format($item->total, 2, ".", ","),$l,0,'R',0);
           $this->pdf->Ln(12);
-          $this->pdf->Line($this->pdf->GetX(),$this->pdf->GetY(),$this->pdf->GetX()+200,$this->pdf->GetY());
+          $this->pdf->Line($this->pdf->GetX(),$this->pdf->GetY()-5,$this->pdf->GetX()+200,$this->pdf->GetY()-5);
         }
+        $this->pdf->SetY($this->pdf->GetY()-5);
         $this->pdf->SetFont('Arial','B',8);
         $this->pdf->Cell(180,5,'Total:','T',0,'R',1); 
         $this->pdf->Cell(20,5,number_format($total, 2, ".", ","),'T',0,'R',1); 
@@ -91,15 +73,36 @@ class Proforma extends CI_Controller {
         if ($proforma->porcentajeDescuento) {
           $sub = 'Descuento ' . $proforma->porcentajeDescuento . '%';
           $this->pdf->Cell(180,5,$sub,'0',0,'R',1); 
-          $this->pdf->Cell(20,5,number_format($proforma->descuento, 2, ".", ","),'T',0,'R',1); 
+          $this->pdf->Cell(20,5,number_format($proforma->descuento, 2, ".", ","),'0',0,'R',1); 
           $this->pdf->Ln(5);
           $this->pdf->Cell(180,5,'Total Final','0',0,'R',1); 
-          $this->pdf->Cell(20,5,number_format($total - $proforma->descuento, 2, ".", ","),'T',0,'R',1); 
+          $this->pdf->Cell(20,5,number_format($total - $proforma->descuento, 2, ".", ","),'TB',0,'R',1); 
         }
+        $entera = intval($proforma->total);
+        $ctvs = round(($proforma->total - $entera) * 100);
+        $ctvs = sprintf('%02d',$ctvs);
+        $ctvs = ($ctvs == 0) ? '00' : $ctvs;
+        $literal = NumeroALetras::convertir($entera).$ctvs.'/100 '.'BOLIVIANOS';
+        $this->pdf->Ln(6);
+        $this->pdf->Cell(9,6,'SON: ',0,0,'L',1);
+        $this->pdf->Cell(188,6,$literal,0,0,'l',1);
+        //$this->pdf->Ln(5);
+        $this->pdf->SetFillColor(20,60,190);
+        $this->pdf->Rect(10,$this->pdf->GetY()+5,200,1,'F');
+        $this->pdf->Ln(6);
+        $glosas = explode('<br />',$proforma->glosa);
+        /* foreach ($glosas as $glosa) {
+          echo($glosa);
+        } */
 
-
-
-
-      
+        //$this->observaciones($proforma->glosa);
+        
+  }
+  public function observaciones($glosas)
+  {
+    foreach ($glosas as $glosa) {
+      $this->pdf->MultiCell(130,5,$glosa,0,'L',0);
+      $this->pdf->SetXY(10,$this->pdf->GetY()-5);
+  }
   }
 }
--- application/libraries/Proforma_lib.php
+++ application/libraries/Proforma_lib.php
@@ -9,83 +9,150 @@
         public function __construct($params){
             parent::__construct();
             $this->datos = $params;
+            
         }
         public function Header() {
-            //$this->setlocale(LC_ALL,"es_ES");
-            $almacen = $this->datos['almacen'];
-            $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-            $fecha = date('d/m/Y',strtotime($this->datos['fecha']));
-
-
              //TITULO
+             $borde = 0;
             $this->Image('images/hergo.jpeg', 10, 8, 55 );
-            $this->SetFont('Arial','B',20);
-            $this->SetXY(143,10);
+            $this->SetFont('Arial','B',18);
+            $this->SetXY(150,8);
             $titulo = 'PROFORMA ' . str_pad($this->datos['num'], 3, '0', STR_PAD_LEFT) .'/' . date('y',strtotime( $this->datos['fecha'] )) ;
-            $this->Cell(70,5, utf8_decode($titulo),0,0,'');
+            $this->Cell(60,10, utf8_decode($titulo),$borde,0,'');
 
-            $this->SetFont('Arial','B',12);
+            $this->SetFont('Arial','B',10);
             $this->SetXY(10,25);
-            $this->Cell(70,5, utf8_decode('NIT: 1000991026'),0,0,'');
+            $this->Cell(50,5, utf8_decode('NIT: 1000991026'),$borde,0,'');
             $this->Ln(5);
-            $this->SetFont('Arial','',10);
-            $this->Cell(70,5, utf8_decode('Av. Montes Nº 611 - Casilla 1024'),0,0,'');
+            $this->SetFont('Arial','',7);
+            $this->Cell(50,5, utf8_decode('Av. Montes Nº 611 - Casilla 1024'),$borde,0,'');
             $this->Ln(5);
-            $this->Cell(70,5, utf8_decode('Telfs: 228 5837 - 228 5854 - Fax 212 6286'),0,0,'');
+            $this->Cell(50,5, utf8_decode('Telfs: 228 5837 - 228 5854 - Fax 212 6286'),$borde,0,'');
             $this->Ln(5);
-            $this->Cell(70,5, utf8_decode('La Paz - Bolivia'),0,0,'');
+            $this->Cell(50,5, utf8_decode('La Paz - Bolivia'),$borde,0,'');
 
             $this->SetFillColor(20,60,190);
-            $this->Rect(80,20,2,30,'F');
-            
-            $this->SetFont('Arial','B',12);
-            $this->SetXY(85,25);
-            $this->Cell(70,5, utf8_decode('PARA: WILLY SALAS'),0,0,'');
-            $this->SetFont('Arial','',10);
-            $this->SetXY(85,30);
-            $this->Cell(70,5, utf8_decode('DIRECCION: LA CALLE DE MI CASA'),0,0,'');
-            $this->SetXY(85,35);
-            $this->Cell(70,5, utf8_decode('TELEFONO: 70572005'),0,0,'');
-            $this->SetXY(85,40);
-            $this->Cell(70,5, utf8_decode('EMAIL: wmx.seo@gmail.com'),0,0,'');
+            $this->Rect(60,25,1,20,'F');
+            $xCliente = 65;
+            $this->SetFont('Arial','B',10);
+            $this->SetXY($xCliente,20);
+            $this->Cell(60,5, utf8_decode('Para: ' . $this->datos['clienteNombre']),$borde,0,'');
+            if (isset($this->datos['clienteTelefono'])) {
+                $this->SetXY($xCliente,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(15,5, utf8_decode('Teléfono:'),$borde,0,'');
+                $this->SetFont('Arial','',8);
+                $this->Cell(45,5, utf8_decode($this->datos['clienteTelefono']),$borde,0,'');
+            }
+            if (isset($this->datos['clienteEmail'])) {
+                $this->SetXY($xCliente,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(10,5, utf8_decode('e-mail:'),$borde,0,'');
+                $this->SetFont('Arial','',8);
+                $this->Cell(50,5, utf8_decode(strtolower($this->datos['clienteEmail'])),$borde,0,'');
+            }
+            if (isset($this->datos['clienteDirec'])) {
+                $this->SetXY($xCliente,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(60,5, utf8_decode('Dirección:'),$borde,0,'');
+                $this->SetXY($xCliente,$this->GetY()+5);
+                $this->SetFont('Arial','',8);
+                $this->Cell(60,5, utf8_decode(ucwords(strtolower($this->datos['clienteDirec']))),$borde,0,'');
+            }
 
+            
             $this->SetFillColor(20,60,190);
-            $this->Rect(150,20,2,30,'F');
+            $this->Rect(125,25,1,20,'F');
 
-            $this->SetFont('Arial','B',12);
-            $this->SetXY(155,25);
-            $this->Cell(70,5, utf8_decode('Fecha : 20/12/2021'),0,0,'');
-            $this->SetFont('Arial','',9);
-            $this->SetXY(155,30);
-            $this->Cell(70,5, utf8_decode('FORMA DE PAGO: CREDITO'),0,0,'');
-            $this->SetXY(155,35);
-            $this->Cell(70,5, utf8_decode('VALIDEZ DE LA OFERTA: 30 DIAS'),0,0,'');
-            $this->SetXY(155,40);
-            $this->Cell(70,5, utf8_decode('LUGAR DE ENTREGA:'),0,0,'');
-            $this->SetXY(155,45);
-            $this->Cell(70,5, utf8_decode(' RECOJO DE TODAS SUCURSALES'),0,0,'');
-
-            
-            
-            $this->Cell(0,15, '',0,0,'C');
-            $this->Ln(10);
-            $this->SetFont('Arial','B',12);
-            $this->Cell(0,10, '',0,0,'C');
-            
+            $this->SetFont('Arial','B',10);
+            $this->SetFillColor(255,255,255);
+            $xData = 128;
+            $this->SetXY($xData,20);
+            $this->Cell(80,5, utf8_decode('Fecha : ' . date('d/m/Y',strtotime($this->datos['fecha'])) ),$borde,0,'',1);
+            if (isset($this->datos['condicionesPago'])) {
+                $this->SetXY($xData,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(25,5, utf8_decode('Forma de Pago: '),$borde,0,'',1);
+                $this->SetFont('Arial','',8);
+                $this->Cell(55,5, utf8_decode($this->datos['condicionesPago']),$borde,0,'',1);
+            }
+            if (isset($this->datos['validezOferta'])) {
+                $this->SetXY($xData,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(25,5, utf8_decode('Validez de Oferta: '),$borde,0,'',1);
+                $this->SetFont('Arial','',8);
+                $this->Cell(55,5, utf8_decode($this->datos['validezOferta']),$borde,0,'',1);
+            }
+            if (isset($this->datos['garantia'])) {
+                $this->SetXY($xData,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(15,5, utf8_decode('Garantia: '),$borde,0,'',1);
+                $this->SetFont('Arial','',8);
+                $this->Cell(65,5, utf8_decode($this->datos['garantia']),$borde,0,'',1);
+            }
+            if (isset($this->datos['tiempoEntrega'])) {
+                $this->SetXY($xData,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(30,5, utf8_decode('Tiempo de Entrega: '),$borde,0,'',1);
+                $this->SetFont('Arial','',8);
+                $this->Cell(50,5, utf8_decode($this->datos['tiempoEntrega']),$borde,0,'',1);
+            }
+            if (isset($this->datos['lugarEntrega'])) {
+                $this->SetXY($xData,$this->GetY()+5);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(25,5, utf8_decode('Lugar de Entrega:'),$borde,0,'');
+                $this->SetFont('Arial','',8);
+                $this->Cell(55,5, utf8_decode($this->datos['lugarEntrega']),$borde,0,'',1);
+            }
+            $l = 0;
+            $this->Ln(5);
+            $this->SetFillColor(20,60,190);
+                $this->SetFont('Arial','B',8); 
+                $this->SetTextColor(255,255,255);
+                  $this->Cell(5,6,utf8_decode('Nº'),$l,0,'C',1);
+                  $this->Cell(25,6,'Imagen',$l,0,'C',1);
+                  $this->Cell(15,6,utf8_decode('Código'),$l,0,'C',1);  //ANCHO,ALTO,TEXTO,BORDE,SALTO DE LINEA, CENTREADO, RELLENO
+                  $this->Cell(45,6,utf8_decode('Descripción'),$l,0,'C',1);
+                  $this->Cell(15,6,'Marca',$l,0,'C',1);
+                  $this->Cell(15,6,'Industria',$l,0,'C',1);
+                  $this->Cell(15,6,'Entrega',$l,0,'C',1);
+                  $this->Cell(15,6,'Cant.',$l,0,'R',1);
+                  $this->Cell(10,6,'Uni.',$l,0,'R',1);
+                  $this->Cell(20,6,'P/U',$l,0,'R',1);
+                  $this->Cell(20,6,'Total',$l,0,'R',1);
+                  $this->Ln(6);
         }
 
         public function Footer(){
+            $this->Rect(10,230,200,1,'F');
+            $this->Rect(145,245,1,30,'F');
 
-            $this->SetLineWidth(0.5);
-            $this->Line(10,260,208,260); //ok
-            
+           
                 //leyendas
-                $this->SetY(260);
+                $glosas = explode('<br />',$this->datos['glosa']);
+               //print_r($glosas);die();
+
+                $this->SetY(235);
+                $this->SetFont('Arial','B',8);
+                $this->Cell(0,4, utf8_decode('OBSERVACIÓNES: '),0,0,'L');
+                $this->Ln(4);
                 $this->SetFont('Arial','',7);
-                $this->Cell(0,4, utf8_decode('glosa01'),0,0,'C');
+                $yyy = 5;
+                foreach ($glosas as $glosa) {
+                    $this->MultiCell(130,5,$glosa,0,'L',0);
+                    $this->SetXY(10,$this->GetY()-5);
+                }
+                $this->SetXY(155,260);
+                    $this->Cell(50,4, utf8_decode('Elaborado por:'),0,0,'C');
+                    $this->Ln(3);
+                    $this->SetX(155);
+                    $this->Cell(50,4, utf8_decode('Willy Salas'),0,0,'C');
+                    $this->Ln(3);
+                    $this->SetX(155);
+                    $this->Cell(50,4, utf8_decode('willy@hergo.com.bo - 75288681'),0,0,'C');
                 //NUMERO PIED PAGINA
                 $this->SetY(270);
-                $this->SetFont('Arial','I', 9);
+                $this->SetFont('Arial','I', 6);
                 $this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
         }
 
