ed54a9eaa16ac0d8f1cb956be83c4d73e65163ad
wilmix <wmx.seo@gmail.com>
2022-02-11 19:19:12 -0400

    arreglo composer añadio vendor a gitignore

---
+++ application/controllers/SpacesDO.php
@@ -0,0 +1,51 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+class SpacesDO extends CI_Controller {
+  public function index()
+  {
+    // Configure a client using Spaces
+    $client = new Aws\S3\S3Client([
+      'version' => 'latest',
+      'region'  => 'nyc3',
+      'endpoint' => 'https://nyc3.digitaloceanspaces.com',
+      'credentials' => [
+              'key'    => 'WSI3HX44FHUFVB74YRYT',
+              'secret' => 'DmoR/t7dkh8oYviqoK9IK3SqpKVozZO9hq9djGpCRgo',
+          ],
+    ]);
+    // Listing all Spaces in the region
+    /*$spaces = $client->listBuckets();
+    foreach ($spaces['Buckets'] as $space){
+        echo $space['Name']."\n";
+    }*/
+
+    
+
+
+    $objects = $client->listObjects([
+      'Bucket' => 'hergo-space'
+    ]);
+
+      //print_r($objects['Contents']);
+      foreach ($objects['Contents'] as $object){
+        echo $object['Key']."</br>";
+      }
+
+
+
+
+    
+    // Upload a file to the Space
+    /*$insert = $client->putObject([
+      'Bucket' => 'hergo-space',
+      'Key'    => 'articulos/file.txt',
+      'Body'   => 'el body.',
+      'ACL' => 'public-read'
+    ]);
+    print_r($insert);*/
+
+  }
+}
