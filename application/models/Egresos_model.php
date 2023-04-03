e8cfff3cd24054bc3e2f658b178804734520b903
wilmix <wmx.seo@gmail.com>
2023-04-03 07:19:34 -0400

    modifico nombre de tabla notaentregasinfo error en escritura

--- application/models/Egresos_model.php
+++ application/models/Egresos_model.php
@@ -702,7 +702,7 @@ class Egresos_model extends CI_Model
 
             if ($egreso->tipomov == 7) {
 				$notaEntrega->egresos_id = $idEgreso;
-                $this->db->insert("notaEntregasInfo", $notaEntrega);
+                $this->db->insert("notaentregasinfo", $notaEntrega);
 			}
             $egresoDetalle = array();
                 foreach ($egreso->articulos as $fila) {
