48dfefb6f8763fab7dd7f37ecc44d5e4ec3e4360
wilmix <wmx.seo@gmail.com>
2023-10-13 12:57:47 -0400

    restructuro factura siat pdf y se añadio multitlinea en razon social

--- application/controllers/pdf/Siat.php
+++ application/controllers/pdf/Siat.php
@@ -1,109 +1,103 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    // Incluimos el archivo fpdf
-    require_once APPPATH."/third_party/fpdf/fpdf.php";
-    require_once APPPATH."/third_party/numerosLetras/NumeroALetras.php";
-    require_once APPPATH."/third_party/multicell/PDF_MC_Table.php";
-class Siat extends CI_Controller {
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+// Incluimos el archivo fpdf
+require_once APPPATH . "/third_party/fpdf/fpdf.php";
+require_once APPPATH . "/third_party/numerosLetras/NumeroALetras.php";
+require_once APPPATH . "/third_party/multicell/PDF_MC_Table.php";
+class Siat extends CI_Controller
+{
   public $Emitir_model;
   public $pdf;
 
-  public function factura($id=null) {
-    //echo $id;
+  public function factura($id = null)
+  {
     $this->load->model('siat/Emitir_model');
     $factura = $this->Emitir_model->getFactura($id);
     $decimales = $factura->lote == '138' ? 5 : 2;
     $lineas = $this->Emitir_model->getDetalleFactura($id, $decimales);
     $params = (array) $factura;
-    //$year = date('y',strtotime($factura->fechaEmision));
-    
+
     /* echo '<pre>';
      print_r($lineas);
      print_r($factura);
     echo '</pre>'; */
-    
+
     $this->load->library('pdf/FacturaSiatLib', $params);
-        $this->pdf = new FacturaSiatLib($params);
-        //print_r(( $params));
-        
-        //$this->pdf->AddPage('P',array(195,216));
-        $this->pdf->AddPage('P','Letter');
-        $this->pdf->AliasNbPages();
-        $this->pdf->SetAutoPageBreak(true,120); //40
-        $this->pdf->SetTitle('FAC' . '-' .$factura->numeroFactura. '-' . $factura->gestion);
-        $this->pdf->SetLeftMargin(10);
-        $this->pdf->SetRightMargin(10);
-        $this->pdf->SetX(10);
-        $this->pdf->SetFont('Arial', '', 7);
-        $this->pdf->SetLineWidth(0.2);
-            
-        $l = '0';
-            $totalFactura=0;
-            foreach ($lineas as $linea) {
-                $totalFactura += $linea->subTotal;
-                $this->pdf->SetFillColor(255,255,255);
-                    // cell(w , h, 'dato', border, ln, align, fill)
-                    $this->pdf->Cell(15,5,$linea->codigo,$l,0,'C',0);
-                    $this->pdf->Cell(15,5,number_format($linea->cantidad, 2, ".", ","),$l,0,'C',0);
-                    $this->pdf->SetFont('Arial', '', 6);
-                    $this->pdf->Cell(20,5,$linea->unidad,$l,0,'C',0); 
-                    $this->pdf->SetFont('Arial', '', 7);
-                    $this->pdf->MultiCell(88,5,utf8_decode($linea->descripcion),$l,'L',0);
-                    $this->pdf->SetXY(148,$this->pdf->GetY()-5);
-                    $this->pdf->Cell(20,5,number_format($linea->precioUnitario, $decimales, ".", ","),$l,0,'R',0);
-                    $this->pdf->Cell(20,5,number_format($linea->descuento, 2, ".", ","),$l,0,'R',0);
-                    $this->pdf->Cell(20,5,number_format(($linea->subTotal), $decimales, ".", ","),$l,0,'R',0);
-                $this->pdf->Ln(5);
+    $this->pdf = new FacturaSiatLib($params);
 
-                $this->pdf->Line(10,$this->pdf->GetY(),208,$this->pdf->GetY());
-            }
-           
-            $this->literal($factura->total,$this->pdf->GetY());
+    $this->pdf->AddPage('P', 'Letter');
+    $this->pdf->AliasNbPages();
+    $this->pdf->SetAutoPageBreak(true, 120); //40
+    $this->pdf->SetTitle('FAC' . '-' . $factura->numeroFactura . '-' . $factura->gestion);
+    $this->pdf->SetLeftMargin(10);
+    $this->pdf->SetRightMargin(10);
+    $this->pdf->SetX(10);
+    $this->pdf->SetFont('Arial', '', 7);
+    $this->pdf->SetLineWidth(0.2);
 
-            $this->totales($totalFactura, $this->pdf->GetY()-5, $factura->montoTotalMoneda, $factura->moneda, $factura->tipoCambio);
+    $l = '0';
+    $totalFactura = 0;
+    foreach ($lineas as $linea) {
+      $totalFactura += $linea->subTotal;
+      $this->pdf->SetFillColor(255, 255, 255);
+      $this->pdf->Cell(15, 5, $linea->codigo, $l, 0, 'C', 0);
+      $this->pdf->Cell(15, 5, number_format($linea->cantidad, 2, ".", ","), $l, 0, 'C', 0);
+      $this->pdf->SetFont('Arial', '', 6);
+      $this->pdf->Cell(20, 5, $linea->unidad, $l, 0, 'C', 0);
+      $this->pdf->SetFont('Arial', '', 7);
+      $this->pdf->MultiCell(88, 5, mb_convert_encoding($linea->descripcion, "ISO-8859-1"), $l, 'L', 0);
+      $this->pdf->SetXY(148, $this->pdf->GetY() - 5);
+      $this->pdf->Cell(20, 5, number_format($linea->precioUnitario, $decimales, ".", ","), $l, 0, 'R', 0);
+      $this->pdf->Cell(20, 5, number_format($linea->descuento, 2, ".", ","), $l, 0, 'R', 0);
+      $this->pdf->Cell(20, 5, number_format(($linea->subTotal), $decimales, ".", ","), $l, 0, 'R', 0);
+      $this->pdf->Ln(5);
 
-        //guardar
-       $this->pdf->Output('I','FAC' . '-' .$factura->numeroFactura. '-' . $factura->gestion .'.pdf',true);
+      $this->pdf->Line(10, $this->pdf->GetY(), 208, $this->pdf->GetY());
+    }
+
+    $this->literal($factura->total, $this->pdf->GetY());
+
+    $this->totales($totalFactura, $this->pdf->GetY() - 5, $factura->montoTotalMoneda, $factura->moneda, $factura->tipoCambio);
+
+    $this->pdf->Output('I', 'FAC' . '-' . $factura->numeroFactura . '-' . $factura->gestion . '.pdf', true);
   }
-  public function literal($total,$y)
+  public function literal($total, $y)
   {
     $this->pdf->SetY($y);
     $l = 0;
-    $entera = intval(round($total,2));
-    $ctvs = round((round($total,2) - $entera) * 100);
-    $ctvs = sprintf('%02d',$ctvs);
+    $entera = intval(round($total, 2));
+    $ctvs = round((round($total, 2) - $entera) * 100);
+    $ctvs = sprintf('%02d', $ctvs);
     $ctvs = ($ctvs == 0) ? '00' : $ctvs;
-    $this->pdf->SetFont('Arial','',8);
-    $this->pdf->Cell(10,5,'SON: ',$l,0,'L',0);
-    $literal = NumeroALetras::convertir($entera).$ctvs.'/100 '.'BOLIVIANOS';
-    //$this->pdf->Cell(113,6,$literal,$l,0,'l',1);
-    $this->pdf->MultiCell(113,5,utf8_decode($literal),$l,'L',0);
+    $this->pdf->SetFont('Arial', '', 8);
+    $this->pdf->Cell(10, 5, 'SON: ', $l, 0, 'L', 0);
+    $literal = NumeroALetras::convertir($entera) . $ctvs . '/100 ' . 'BOLIVIANOS';
+    $this->pdf->MultiCell(113, 5, mb_convert_encoding($literal, "ISO-8859-1"), $l, 'L', 0);
   }
   public function totales($totalFactura, $y, $montoTotalMoneda, $moneda, $tipoCambio)
   {
     $this->pdf->SetY($y);
-    $this->pdf->SetFont('Arial','',8);
-    $this->pdf->SetFillColor(255,255,255);
+    $this->pdf->SetFont('Arial', '', 8);
+    $this->pdf->SetFillColor(255, 255, 255);
     $this->pdf->SetX(133);
-    $this->pdf->Cell(55,5,utf8_decode('SUBTOTAL Bs'),'TL',0,'R',1);
-    $this->pdf->Cell(20,5,number_format($totalFactura, 2, ".", ","),'T',1,'R',1);
+    $this->pdf->Cell(55, 5, mb_convert_encoding('SUBTOTAL Bs', "ISO-8859-1"), 'TL', 0, 'R', 1);
+    $this->pdf->Cell(20, 5, number_format($totalFactura, 2, ".", ","), 'T', 1, 'R', 1);
     $this->pdf->SetX(133);
-    $this->pdf->Cell(55,5,utf8_decode('DESCUENTO Bs'),'TL',0,'R',1);
-    $this->pdf->Cell(20,5,number_format(0, 2, ".", ","),'T',1,'R',1); 
+    $this->pdf->Cell(55, 5, mb_convert_encoding('DESCUENTO Bs', "ISO-8859-1"), 'TL', 0, 'R', 1);
+    $this->pdf->Cell(20, 5, number_format(0, 2, ".", ","), 'T', 1, 'R', 1);
     $this->pdf->SetX(133);
-    $this->pdf->SetFont('Arial','B',8);
-    $this->pdf->Cell(55,5,'MONTO A PAGAR Bs','TL',0,'R',1);
-    $this->pdf->Cell(20,5,number_format($totalFactura, 2, ".", ","),'T',1,'R',1);
+    $this->pdf->SetFont('Arial', 'B', 8);
+    $this->pdf->Cell(55, 5, 'MONTO A PAGAR Bs', 'TL', 0, 'R', 1);
+    $this->pdf->Cell(20, 5, number_format($totalFactura, 2, ".", ","), 'T', 1, 'R', 1);
     if ($moneda == 2) {
       $this->pdf->SetX(133);
-      $this->pdf->Cell(55,5,utf8_decode('MONTO A PAGAR (DOLAR)'),'TLB',0,'R',1);
-      $this->pdf->Cell(20,5,number_format($montoTotalMoneda, 2, ".", ","),'TB',1,'R',1); 
+      $this->pdf->Cell(55, 5, mb_convert_encoding('MONTO A PAGAR (DÓLAR)', "ISO-8859-1"), 'TLB', 0, 'R', 1);
+      $this->pdf->Cell(20, 5, number_format($montoTotalMoneda, 2, ".", ","), 'TB', 1, 'R', 1);
       $this->pdf->SetX(133);
-      $this->pdf->Cell(55,5,utf8_decode('TIPO DE CAMBIO'),'TLB',0,'R',1);
-      $this->pdf->Cell(20,5,number_format($tipoCambio, 2, ".", ","),'TB',1,'R',1);   
+      $this->pdf->Cell(55, 5, mb_convert_encoding('TIPO DE CAMBIO', "ISO-8859-1"), 'TLB', 0, 'R', 1);
+      $this->pdf->Cell(20, 5, number_format($tipoCambio, 2, ".", ","), 'TB', 1, 'R', 1);
     }
     $this->pdf->SetX(133);
-    $this->pdf->Cell(55,5,utf8_decode('IMPORTE BASE CRÉDITO FISCAL Bs'),'TLB',0,'R',1);
-    $this->pdf->Cell(20,5,number_format($totalFactura, 2, ".", ","),'TB',1,'R',1);  
-    
+    $this->pdf->Cell(55, 5, mb_convert_encoding('IMPORTE BASE CRÉDITO FISCAL Bs', "ISO-8859-1"), 'TLB', 0, 'R', 1);
+    $this->pdf->Cell(20, 5, number_format($totalFactura, 2, ".", ","), 'TB', 1, 'R', 1);
   }
 }
--- application/libraries/pdf/FacturaSiatLib.php
+++ application/libraries/pdf/FacturaSiatLib.php
@@ -1,202 +1,175 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    // Incluimos el archivo fpdf
-    require_once APPPATH."/third_party/fpdf/fpdf.php";
-    require_once APPPATH."/third_party/phpqrcode/qrlib.php";
-    require_once APPPATH."/third_party/multicell/PDF_MC_Table.php";
-    //Extendemos la clase Pdf de la clase fpdf para que herede todas sus variables y funciones
-    class FacturaSiatLib extends FPDF {
-        private $datos = array();
-        public function __construct($params){
-            parent::__construct();
-            $this->datos = $params;
-        }
-        public function Header() {
-             //TITULO
-            $l = '0';
-            $this->SetXY(10,9);
-            $this->Image('images/hergo.jpeg', 15, 8, 45 );
-            $this->SetFont('Arial','B',24);
-            $this->SetTextColor(0,0,200);
-            $this->Cell(0,15, '',0,0,'C');
-            $this->Ln(10);
-            $this->SetFont('Arial','B',12);
-            $this->Cell(0,10, '',0,0,'C');
-
-            //SUCURSAL
-            $this->SetXY(10,18);
-            //$this->SetTextColor(0,0,0);
-            /* if ($this->datos['sucursal'] == 'Casa Matriz - 0') {
-                $this->Ln(2);
-            } */
-            $this->Ln(2);
-
-            $this->SetFont('Arial','B',7);
-           
-            /* if ($this->datos['sucursal'] != 'Casa Matriz') {
-                $this->SetFont('Arial','B',7);
-                $this->Cell(60,4,utf8_decode($this->datos['sucursal']),0,1,'C');
-                $this->SetFont('Arial','',5);
-            } */
-            $this->SetFont('Arial','B',7);
-            $this->Cell(60,4,utf8_decode($this->datos['sucursal']),0,1,'C');
-            $this->SetFont('Arial','',6);
-            $this->Cell(60,3,utf8_decode('No. Punto de Venta: ' . $this->datos['codigoPuntoVenta']),0,1,'C');
-            $this->Cell(60,3,utf8_decode($this->datos['direccion']),0,1,'C');
-            $this->Cell(60,3,utf8_decode('Teléfono: ' . $this->datos['telefono']),0,1,'C');
-            $this->Cell(60,3,utf8_decode($this->datos['ciudad']),0,1,'C');
-
-
-            //n FACTURA DERECHA
-            $this->SetXY(138,10);
-            $this->SetFont('Arial','B',14);
-            $this->SetTextColor(0,0,200);
-            $this->Cell(70,7,'NIT: '.'1000991026',0,1,'C');
-            $this->SetXY(138,17);
-            $this->SetFont('Arial','B',8);
-            $this->SetTextColor(0,0,0);
-            $this->Cell(32,8, utf8_decode('FACTURA N°:'),0,0,'R');
-            $this->SetFont('Arial','B',14);
-            $this->Cell(35,8, utf8_decode($this->datos['numeroFactura']),0,0,'C');
-            $this->SetXY(138,25);
-            $this->SetFont('Arial','B',7);
-            $this->Cell(32,8, utf8_decode('CÓD. AUTORIZACIÓN:'),0,0,'R');
-            $this->SetFont('Arial','B',7);
-            //$this->Cell(35,8, utf8_decode($this->datos['cuf']),1,0,'C');
-            $this->MultiCell(35,4,utf8_decode($this->datos['cuf']),0,'L',0);
-            //$this->SetXY(168,$this->pdf->GetY()-5);
-            $this->SetXY(138,33);
-            $this->SetFont('Arial','I',7);
-            //$this->MultiCell(70,5, utf8_decode($glosa03),0,'C',false);
-
-            
-                //****ENCABEZADO****
-                $this->SetXY(10,42);
-                $fontSize = 9;
-                //cliente
-                $this->SetFont('Arial','B',$fontSize);
-                $this->Cell(20,6, utf8_decode('Fecha: '),$l,0,'');
-                $this->SetFont('Arial','',$fontSize);
-                $this->Cell(40, 6, utf8_decode($this->datos['fechaEmision']),$l,0,'L');
-                $this->SetFont('Arial','B',$fontSize);
-                if ($this->datos['pedido'] ==! '') {
-                    
-                    $this->Cell(15,6, utf8_decode('Pedido: '),$l,0,'');
-                    $this->SetFont('Arial','',$fontSize);
-                    /* if ($this->datos['complemento']) {
-                    $this->Cell(55, 6, utf8_decode($this->datos['pedido']),$l,0,'L');
-                    } else { */
-                        $this->Cell(68, 6, utf8_decode($this->datos['pedido']),$l,0,'L');
-                    /* } */
-                } else {
-                   /*  if ($this->datos['complemento']) {
-                    $this->Cell(15,6, '',$l,0,'');
-                    $this->Cell(55,6, '',$l,0,'');
-                    } else { */
-                        $this->Cell(15,6, '',$l,0,'');
-                        $this->Cell(68,6, '',$l,0,'');
-                    /* } */
-
-                }
-                $this->SetFont('Arial','B',$fontSize);
-                $this->Cell(20,6, utf8_decode('NIT/CI/CEX:'),$l,0,'');
-                $this->SetFont('Arial','',$fontSize);
-                
-                if ($this->datos['complemento']) {
-                    /* $this->SetFont('Arial','B',$fontSize);
-                    $this->Cell(10,6, utf8_decode('-'),$l,0,''); */
-                    $this->SetFont('Arial','',$fontSize);
-                    $this->Cell(35, 6, utf8_decode($this->datos['documentoNumero'] . ' - ' . $this->datos['complemento']), $l,0,'R');
-                    //$this->Cell(6, 6, utf8_decode(' - ' . $this->datos['complemento'] ), $l,0,'R');
-                } else {
-                    $this->Cell(35, 6, utf8_decode($this->datos['documentoNumero']), $l,0,'R');
-                }
-                $this->Ln(6);
-
-                $this->SetX(10);
-                $this->SetFont('Arial','B',$fontSize);
-                $this->Cell(35,6, utf8_decode('Nombre/Razon Social: '),$l,0,'');
-                $this->SetFont('Arial','',$fontSize);
-                $this->Cell(108, 6, utf8_decode($this->datos['nombreRazonSocial']),$l,0,'L');
-                $this->SetFont('Arial','B',$fontSize);
-                $this->Cell(20,6, utf8_decode('Cod. Cliente:'),$l,0,'');
-                $this->SetFont('Arial','',$fontSize);
-                $this->Cell(35, 6, utf8_decode($this->datos['codigoCliente']),$l,0,'R');
-                
-                $this->Ln(8);
-
-                
-
-                
-
-                    //ENCABEZADO TABLA
-                    $this->SetX(15);
-                    $this->Ln(1);
-                    $this->SetFillColor(235,235,235);
-                    $f = '0';
-                    $x = 25;
-                    $this->SetLineWidth(0.5);
-                    $this->Line(10,57,208,57);
-
-                    $this->SetFont('Arial','B',6); 
-                    $this->MultiCell(15,3,utf8_decode('CÓDIGO PRODUCTO / SERVICIO'),$l,'C',$f);
-                    $this->SetXY($x,$this->GetY()-5);
-                    $this->MultiCell(15,3,utf8_decode('CANTIDAD'),$l,'C',$f);
-                    $this->SetXY($x+=15,$this->GetY()-5);
-                    $this->MultiCell(20,3,utf8_decode('UNIDAD DE MEDIDA'),$l,'C',$f);
-                    $this->SetXY($x+=20,$this->GetY()-5);
-                    $this->MultiCell(88,3,utf8_decode('DESCRIPCIÓN'),$l,'C',$f);
-                    $this->SetXY($x+=88,$this->GetY()-5);
-                    $this->MultiCell(20,3,utf8_decode('PRECIO UNITARIO'),$l,'R',$f);
-                    $this->SetXY($x+=20,$this->GetY()-5);
-                    $this->MultiCell(20,3,utf8_decode('DESCUENTO'),$l,'R',$f);
-                    $this->SetXY($x+=20,$this->GetY()-3);
-                    $this->MultiCell(20,3,utf8_decode('SUBTOTAL'),$l,'R',$f);
-
-                    $this->Line(10,66,208,66);
-                    $this->Ln(4);
-        }
-        public function Footer(){
-            $l = '0';
-            $y = -110;
-            $this->SetLineWidth(0.5);
-            $this->Line(10,158,208,158); //ok
-                //leyendas
-                $this->SetFont('Arial','',7);
-                if ($this->datos['glosa'] !== '') {
-                    $this->SetY($y);
-                    $this->SetFont('Arial','B',7);
-                    $this->Cell(15,4, utf8_decode('GLOSA:'),$l,0,'L');
-                    $this->SetFont('Arial','',7);
-                    $this->Cell(145,4, utf8_decode($this->datos['glosa']),$l,0,'L');
-                }
-                $this->SetY($y+4);
-                $this->Cell(160,4, utf8_decode('ESTA FACTURA CONTRIBUYE AL DESARROLLO DEL PAÍS, EL USO ILÍCITO SERÁ SANCIONADO PENALMENTE DE ACUERDO A LEY'),$l,0,'C');
-                $this->SetY($y+8);
-                $this->MultiCell(160,3,utf8_decode($this->datos['leyenda']),$l,'C',0);
-                $this->SetXY(10,$this->GetY());
-                if ($this->datos['codigoEmision'] == '2') {
-                    $this->MultiCell(160,4,utf8_decode('Este documento es la Representación Gráfica de un Documento Fiscal Digital emitido fuera de línea, verifique su envío con su proveedor o en la página web www.impuestos.gob.bo'),$l,'C',0);
-                } else {
-                    $this->Cell(160,4, utf8_decode('Este documento es la Representación Gráfica de un Documento Fiscal Digital emitido en una modalidad de facturación en línea'),$l,0,'C');
-                }
-                
-
-                //NUMERO PIED PAGINA
-                $this->SetY($y+18);
-                $this->SetFont('Arial','I', 7);
-                $this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
-                //QR
-                $this->SetXY(170, $y);
-                $this->SetFont('Arial','',7);
-                $nit = '1000991026';
-                $cuf = $this->datos['cuf'];
-                $numeroFactura = $this->datos['numeroFactura'];
-                $QR = "https://siat.impuestos.gob.bo/consulta/QR?nit=1000991026&cuf=$cuf&numero=$numeroFactura&t=2";
-                $nameQR ='qr/' . $this->datos['cuf'] . '.png';
-                QRcode::png($QR, $nameQR );
-                $this->Image($nameQR, 176, 160, 30,0, 'png');
-        }
-
-
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+// Incluimos el archivo fpdf
+require_once APPPATH . "/third_party/fpdf/fpdf.php";
+require_once APPPATH . "/third_party/phpqrcode/qrlib.php";
+require_once APPPATH . "/third_party/multicell/PDF_MC_Table.php";
+//Extendemos la clase Pdf de la clase fpdf para que herede todas sus variables y funciones
+class FacturaSiatLib extends FPDF
+{
+    private $datos = array();
+    public function __construct($params)
+    {
+        parent::__construct();
+        $this->datos = $params;
     }
-?>
+    public function Header()
+    {
+        /*** imagen ***/
+        $l = '0';
+        $this->SetXY(10, 9);
+        $this->Image('images/hergo.jpeg', 15, 8, 45);
+        /*** titulo ***/
+        $this->SetFont('Arial', 'B', 24);
+        $this->SetTextColor(0, 0, 200);
+            $this->Cell(0, 15, '', 0, 0, 'C');
+        $this->Ln(10);
+        /*** subtitulo ***/
+        $this->SetFont('Arial', 'B', 12);
+            $this->Cell(0, 10, '', 0, 0, 'C');
+
+        /*** izquierda***/
+        $this->SetXY(10, 18);
+        $this->Ln(2);
+        $this->SetFont('Arial', 'B', 7);
+            $this->Cell(60, 4, mb_convert_encoding($this->datos['sucursal'], "ISO-8859-1"), 0, 1, 'C');
+        $this->SetFont('Arial', '', 6);
+            $this->Cell(60, 3, mb_convert_encoding('No. Punto de Venta: ' . $this->datos['codigoPuntoVenta'], "ISO-8859-1"), 0, 1, 'C');
+            $this->Cell(60, 3, mb_convert_encoding($this->datos['direccion'], "ISO-8859-1"), 0, 1, 'C');
+            $this->Cell(60, 3, mb_convert_encoding('Teléfono: ' . $this->datos['telefono'], "ISO-8859-1"), 0, 1, 'C');
+            $this->Cell(60, 3, mb_convert_encoding($this->datos['ciudad'], "ISO-8859-1"), 0, 1, 'C');
+
+
+        /*** derecha***/
+        $this->SetXY(138, 10);
+        $this->SetFont('Arial', 'B', 14);
+        $this->SetTextColor(0, 0, 200);
+            $this->Cell(70, 7, 'NIT: ' . '1000991026', 0, 1, 'C');
+        $this->SetXY(138, 17);
+        $this->SetFont('Arial', 'B', 8);
+        $this->SetTextColor(0, 0, 0);
+            $this->Cell(32, 8, mb_convert_encoding('FACTURA N°:', "ISO-8859-1"), 0, 0, 'R');
+        $this->SetFont('Arial', 'B', 14);
+            $this->Cell(35, 8, mb_convert_encoding($this->datos['numeroFactura'], "ISO-8859-1"), 0, 0, 'C');
+        $this->SetXY(138, 25);
+        $this->SetFont('Arial', 'B', 7);
+            $this->Cell(32, 8, mb_convert_encoding('CÓD. AUTORIZACIÓN:', "ISO-8859-1"), 0, 0, 'R');
+        $this->SetFont('Arial', 'B', 7);
+            $this->MultiCell(35, 4, mb_convert_encoding($this->datos['cuf'], "ISO-8859-1"), 0, 'L', 0);
+
+
+
+        /*** datos ***/
+        $this->SetXY(10, 42);
+        $fontSize = 9;
+        $this->SetFont('Arial', 'B', $fontSize);
+            $this->Cell(20, 6, mb_convert_encoding('Fecha: ', "ISO-8859-1"), $l, 0, '');
+        $this->SetFont('Arial', '', $fontSize);
+            $this->Cell(40, 6, mb_convert_encoding($this->datos['fechaEmision'], "ISO-8859-1"), $l, 0, 'L');
+        $this->SetFont('Arial', 'B', $fontSize);
+        if ($this->datos['pedido'] == !'') {
+                $this->Cell(15, 6, mb_convert_encoding('Pedido: ', "ISO-8859-1"), $l, 0, '');
+            $this->SetFont('Arial', '', $fontSize);
+                $this->Cell(68, 6, mb_convert_encoding($this->datos['pedido'], "ISO-8859-1"), $l, 0, 'L');
+        } else {
+            $this->Cell(15, 6, '', $l, 0, '');
+            $this->Cell(68, 6, '', $l, 0, '');
+        }
+        $this->SetFont('Arial', 'B', $fontSize);
+            $this->Cell(20, 6, mb_convert_encoding('NIT/CI/CEX:', "ISO-8859-1"), $l, 0, '');
+        $this->SetFont('Arial', '', $fontSize);
+
+        if ($this->datos['complemento']) {
+            $this->SetFont('Arial', '', $fontSize);
+                $this->Cell(35, 6, mb_convert_encoding($this->datos['documentoNumero'] . ' - ' . $this->datos['complemento'], "ISO-8859-1"), $l, 0, 'R');
+        } else {
+            $this->Cell(35, 6, mb_convert_encoding($this->datos['documentoNumero'], "ISO-8859-1"), $l, 0, 'R');
+        }
+        $this->Ln(6);
+        $this->SetX(10);
+        $this->SetFont('Arial', 'B', $fontSize);
+            $this->Cell(35, 6, mb_convert_encoding('Nombre/Razon Social: ', "ISO-8859-1"), $l, 0, '');
+        $this->SetFont('Arial', '', $fontSize);
+        if (strlen($this->datos['nombreRazonSocial']) > 55) {
+            $this->MultiCell(108, 4, mb_convert_encoding($this->datos['nombreRazonSocial'], "ISO-8859-1"), $l, 'L', 0);
+            $this->SetXY(153, $this->GetY() - 8);
+        } else {
+            $this->MultiCell(108, 6, mb_convert_encoding($this->datos['nombreRazonSocial'], "ISO-8859-1"), $l, 'L', 0);
+            $this->SetXY(153, $this->GetY() - 6);
+        }
+        $this->SetFont('Arial', 'B', $fontSize);
+            $this->Cell(20, 6, mb_convert_encoding('Cod. Cliente:', "ISO-8859-1"), $l, 0, '');
+        $this->SetFont('Arial', '', $fontSize);
+            $this->Cell(35, 6, mb_convert_encoding($this->datos['codigoCliente'], "ISO-8859-1"), $l, 0, 'R');
+        $this->Ln(8);
+
+
+        /*** cabezera tabla ***/
+        $this->SetX(15);
+        $this->Ln(1);
+        $this->SetFillColor(235, 235, 235);
+        $f = '0';
+        $x = 25;
+        $this->SetLineWidth(0.5);
+        $this->Line(10, 57, 208, 57);
+
+        $this->SetFont('Arial', 'B', 6);
+            $this->MultiCell(15, 3, mb_convert_encoding('CÓDIGO PRODUCTO / SERVICIO', "ISO-8859-1"), $l, 'C', $f);
+        $this->SetXY($x, $this->GetY() - 5);
+            $this->MultiCell(15, 3, mb_convert_encoding('CANTIDAD', "ISO-8859-1"), $l, 'C', $f);
+        $this->SetXY($x += 15, $this->GetY() - 5);
+            $this->MultiCell(20, 3, mb_convert_encoding('UNIDAD DE MEDIDA', "ISO-8859-1"), $l, 'C', $f);
+        $this->SetXY($x += 20, $this->GetY() - 5);
+            $this->MultiCell(88, 3, mb_convert_encoding('DESCRIPCIÓN', "ISO-8859-1"), $l, 'C', $f);
+        $this->SetXY($x += 88, $this->GetY() - 5);
+            $this->MultiCell(20, 3, mb_convert_encoding('PRECIO UNITARIO', "ISO-8859-1"), $l, 'R', $f);
+        $this->SetXY($x += 20, $this->GetY() - 5);
+            $this->MultiCell(20, 3, mb_convert_encoding('DESCUENTO', "ISO-8859-1"), $l, 'R', $f);
+        $this->SetXY($x += 20, $this->GetY() - 3);
+            $this->MultiCell(20, 3, mb_convert_encoding('SUBTOTAL', "ISO-8859-1"), $l, 'R', $f);
+
+        $this->Line(10, 66, 208, 66);
+        $this->Ln(4);
+    }
+    public function Footer()
+    {
+        $l = '0';
+        $y = -110;
+        $this->SetLineWidth(0.5);
+        $this->Line(10, 158, 208, 158); //ok
+        /*** glosa ***/
+        $this->SetFont('Arial', '', 7);
+        if ($this->datos['glosa'] !== '') {
+            $this->SetY($y);
+            $this->SetFont('Arial', 'B', 7);
+                $this->Cell(15, 4, mb_convert_encoding('GLOSA:', "ISO-8859-1"), $l, 0, 'L');
+            $this->SetFont('Arial', '', 7);
+                $this->Cell(145, 4, mb_convert_encoding($this->datos['glosa'], "ISO-8859-1"), $l, 0, 'L');
+        }
+        /*** leyendas ***/
+        $this->SetY($y + 4);
+            $this->Cell(160, 4, mb_convert_encoding('ESTA FACTURA CONTRIBUYE AL DESARROLLO DEL PAÍS, EL USO ILÍCITO SERÁ SANCIONADO PENALMENTE DE ACUERDO A LEY', "ISO-8859-1"), $l, 0, 'C');
+        $this->SetY($y + 8);
+            $this->MultiCell(160, 3, mb_convert_encoding($this->datos['leyenda'], "ISO-8859-1"), $l, 'C', 0);
+        $this->SetXY(10, $this->GetY());
+        if ($this->datos['codigoEmision'] == '2') {
+            $this->MultiCell(160, 4, mb_convert_encoding('Este documento es la Representación Gráfica de un Documento Fiscal Digital emitido fuera de línea, verifique su envío con su proveedor o en la página web www.impuestos.gob.bo', "ISO-8859-1"), $l, 'C', 0);
+        } else {
+            $this->Cell(160, 4, mb_convert_encoding('Este documento es la Representación Gráfica de un Documento Fiscal Digital emitido en una modalidad de facturación en línea', "ISO-8859-1"), $l, 0, 'C');
+        }
+
+
+        /*** número de página ***/
+        $this->SetY($y + 18);
+        $this->SetFont('Arial', 'I', 7);
+            $this->Cell(0, 10, 'Pagina ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+        /*** qr ***/
+        $this->SetXY(170, $y);
+        $this->SetFont('Arial', '', 7);
+        $cuf = $this->datos['cuf'];
+        $numeroFactura = $this->datos['numeroFactura'];
+        $QR = "https://siat.impuestos.gob.bo/consulta/QR?nit=1000991026&cuf=$cuf&numero=$numeroFactura&t=2";
+        $nameQR = 'qr/' . $this->datos['cuf'] . '.png';
+        QRcode::png($QR, $nameQR);
+        $this->Image($nameQR, 176, 160, 30, 0, 'png');
+    }
+}
