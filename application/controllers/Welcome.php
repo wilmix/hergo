12896d285216e43371741554519114922f397df6
wilmix <wmx.seo@gmail.com>
2023-12-29 16:08:58 -0400

    welcome identificar traspasos costos

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	public $Welcome_model;
 	public function __construct()
 	{
 		parent::__construct();
@@ -16,6 +17,7 @@ class Welcome extends CI_Controller {
 		$n = 0;
 		foreach ($result as $value) {
 			if ($value->idDetalle == '') {
+				$dateNegative = null;
 				continue;
 			}
 			if ($value->operacion == '+' && $value->tipo == 'II') {
@@ -41,5 +43,51 @@ class Welcome extends CI_Controller {
 			$dateNegative = null;
 			$data =[];
 		}
+	}
+	public function identificarEgresoTraspaso($articulo): array  {
+		$almacen = $almacen = '1,2,3,4,5,6,7,8,9,10';
+		$result = $this->Welcome_model->kardex($almacen, $articulo);
+		return $result;
+		//echo json_encode(print_r($result));
+	}
+	function identificarCostosEgresoTraspaso($articulo): void {
+		$resultLog = [];
+		$egresosTraspasos = $this->identificarEgresoTraspaso($articulo);
+		//print_r($egresosTraspasos);
+		foreach ($egresosTraspasos as $key => $egresoTraspaso) {
+			$egreso = $this->Welcome_model->kardex( $egresoTraspaso->almacen_id, $articulo);
+			foreach ($egreso as $dato) {
+				if ($dato->id == $egresoTraspaso->id) {
+					//print_r($dato); return;
+					$traspaso = $this->Welcome_model->encontrarTraspaso($dato->id);
+					//print_r($traspaso); return;
+					$egresoDetalle = $this->Welcome_model->getEgreso($traspaso[0]['idEgreso'], $dato->idArticulo, $dato->cantidad, $dato->cpp);
+					$ingresoDetalle = $this->Welcome_model->getIngreso($traspaso[0]['idIngreso'], $dato->idArticulo, $dato->cantidad, $dato->cpp);
+					//print_r($egresoDetalle[0]->egreso_id_detalle); return;
+					/* var_dump(floatval($egresoDetalle[0]->nuevo_punitario));
+					var_dump(floatval($egresoDetalle[0]->punitario)); */
+					if (floatval($egresoDetalle[0]->nuevo_punitario) !== floatval($egresoDetalle[0]->punitario)) {
+						$this->Welcome_model->actualizarEgresoDetalle($traspaso[0]['idEgreso'], $dato->idArticulo, $dato->cantidad, $dato->cpp);
+						$this->Welcome_model->actualizarIngresoDetalle($traspaso[0]['idIngreso'], $dato->idArticulo, $dato->cantidad, $dato->cpp);
+						// Registrar la actualización en el log
+						$logEntry = [
+
+							'egreso_id_detalle' => $egresoDetalle[0]->egreso_id_detalle,
+							'ingreso_id_detalle' => $ingresoDetalle[0]->ingreso_id_detalle,
+							'antiguo_punitario' => $dato->punitario,
+							'nuevo_punitario' => $dato->cpp
+							// Agregar más campos según sea necesario
+						];
+	
+						$resultLog[] = $logEntry;
+						
+					} else {
+						$resultLog[] = 'no se modifico';
+					}
+				}
+			}
+			//break;
+		}
+		print_r($resultLog);
 	}
 }
--- application/models/Welcome_model.php
+++ application/models/Welcome_model.php
@@ -23,4 +23,117 @@ class Welcome_model extends CI_Model
 		$this->db->where('idIngresos', $id);
 		$this->db->update('ingresos', $data);
 	}
+	public function kardex($almacenes, $articulo) {
+		 // Formatear la cadena SQL con los valores proporcionados
+		$sql = "CALL newKardex(" . $this->db->escape($almacenes) . ", '0', '2023-01-01', '2023-12-31', " . $this->db->escape($articulo) . ")";
+    	$query = $this->db->query($sql, array($almacenes, $articulo));
+        
+		mysqli_next_result( $this->db->conn_id );
+		$query=$this->db->query($sql);	
+		$result = $query->result();
+		$query->next_result(); 
+		$query->free_result();
+
+		if ($result) {
+			// Construir la consulta adicional
+			$this->db->select(' almacen_id,
+								id, 
+								fechakardex,
+								idArticulo, 
+								idDetalle, 
+								cantidad, 
+								punitario,
+								_auxCOPP2 cpp,
+								(cantidad * _auxCOPP2) totalCorrecto,
+								(cantidad * punitario) as totalActual');
+			$this->db->from('kardex3');
+			$this->db->where('idDetalle IS NOT NULL');
+			$this->db->where('tipo', 'ET');
+			$this->db->where('operacion', '-');
+	
+			// Ejecutar la consulta adicional
+			$kardex = $this->db->get();
+		}
+
+		return $kardex->result();
+	}
+	public function encontrarTraspaso($egreso_id) : array {
+		$sql="	SELECT
+					*
+				FROM
+					traspasos t
+				WHERE
+					t.idEgreso = '$egreso_id';";
+
+        $traspaso=$this->db->query($sql);
+
+		return $traspaso->result_array();
+	}
+	public function getEgreso($egreso_id, $articulo_id, $cantidad, $cpp) : array {
+		$sql="	SELECT
+					ed.idegreso,
+					ed.idingdetalle egreso_id_detalle,
+					ed.articulo,
+					ed.cantidad,
+					ed.punitario,
+					ed.total,
+					$cpp nuevo_punitario,
+					($cpp * ed.cantidad) nuevo_total
+
+				FROM
+					egredetalle ed
+				WHERE
+					ed.idegreso = '$egreso_id'
+					AND ed.articulo = '$articulo_id'
+					AND ed.cantidad = '$cantidad';";
+
+        $traspaso=$this->db->query($sql);
+
+		return $traspaso->result();
+	}
+	public function getIngreso($ingreso_id, $articulo_id, $cantidad, $cpp) : array {
+		$sql="	SELECT
+					id.idIngreso,
+					id.idingdetalle ingreso_id_detalle,
+					id.articulo,
+					id.cantidad,
+					id.punitario,
+					id.total,
+					id.totaldoc,
+					$cpp nuevo_punitario,
+					($cpp * id.cantidad) nuevo_total
+				FROM
+					ingdetalle id
+				WHERE
+					id.idIngreso = '$ingreso_id'
+					AND id.articulo = '$articulo_id'
+					AND id.cantidad = '$cantidad';";
+
+        $traspaso=$this->db->query($sql);
+
+		return $traspaso->result();
+	}
+	public function actualizarEgresoDetalle($egreso_id, $articulo_id, $cantidad, $cpp) {
+        $data = array(
+            'punitario' => $cpp,
+            'total' => $cpp * $cantidad
+        );
+
+        $this->db->where('idegreso', $egreso_id);
+        $this->db->where('articulo', $articulo_id);
+        $this->db->where('cantidad', $cantidad);
+        $this->db->update('egredetalle', $data);
+    }
+	public function actualizarIngresoDetalle($ingreso_id, $articulo_id, $cantidad, $cpp) {
+        $data = array(
+            'punitario' => $cpp,
+            'total' => $cpp * $cantidad,
+			'totaldoc' => $cpp * $cantidad
+        );
+
+        $this->db->where('idIngreso', $ingreso_id);
+        $this->db->where('articulo', $articulo_id);
+        $this->db->where('cantidad', $cantidad);
+        $this->db->update('ingdetalle', $data);
+    }
 }
